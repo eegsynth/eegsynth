1e188d086a2dc81b027429aac90164e497860371
Robert Oostenveld <r.oostenveld@gmail.com>
2015-05-03 09:53:31 +0200

    extended it to a full 88-key keyboard, I posted a demo on youtube https://www.youtube.com/watch?v=XMcx2CLJh5M

--- digitalpiano.m
+++ digitalpiano.m
@@ -3,6 +3,9 @@ function digitalpiano(cfg)
 % DIGITALPIANO create a GUI with a digital piano that plays the notes using MIDI or
 % using the soundcard.
 %
+% This interface has been designed for and tested with the Yamaha P95
+% digital piano, but should also work with any other 88-key MIDI controller.
+%
 % Copyright (C) 2015, Robert Oostenveld
 
 % This file is part of EEGSYNTH, see https://github.com/oostenveld/eegsynth-matlab
@@ -42,26 +45,23 @@ cfg.playsound = ft_getopt(cfg, 'playsound', 'no');
 % this table contains the UI tag, channel and note
 cfg.mapping = cell(120,3);
 
-for i=1:10
-  cfg.mapping((i-1)*12+1,  :) = {sprintf('%d_C' , i), 0, (i+1)*12+0};
-  cfg.mapping((i-1)*12+2,  :) = {sprintf('%d_Db', i), 0, (i+1)*12+1};
-  cfg.mapping((i-1)*12+3,  :) = {sprintf('%d_D' , i), 0, (i+1)*12+2};
-  cfg.mapping((i-1)*12+4,  :) = {sprintf('%d_Eb', i), 0, (i+1)*12+3};
-  cfg.mapping((i-1)*12+5,  :) = {sprintf('%d_E' , i), 0, (i+1)*12+4};
-  cfg.mapping((i-1)*12+6,  :) = {sprintf('%d_F' , i), 0, (i+1)*12+5};
-  cfg.mapping((i-1)*12+7,  :) = {sprintf('%d_Gb', i), 0, (i+1)*12+6};
-  cfg.mapping((i-1)*12+8,  :) = {sprintf('%d_G' , i), 0, (i+1)*12+7};
-  cfg.mapping((i-1)*12+9,  :) = {sprintf('%d_Ab', i), 0, (i+1)*12+8};
-  cfg.mapping((i-1)*12+10, :) = {sprintf('%d_A' , i), 0, (i+1)*12+9};
-  cfg.mapping((i-1)*12+11, :) = {sprintf('%d_Bb', i), 0, (i+1)*12+10};
-  cfg.mapping((i-1)*12+12, :) = {sprintf('%d_B' , i), 0, (i+1)*12+11};
+for i=1:11
+  % see http://en.wikipedia.org/wiki/Scientific_pitch_notation
+  cfg.mapping((i-1)*12+1,  :) = {sprintf('%d_C' , i-1), 0, i*12+0};
+  cfg.mapping((i-1)*12+2,  :) = {sprintf('%d_Db', i-1), 0, i*12+1};
+  cfg.mapping((i-1)*12+3,  :) = {sprintf('%d_D' , i-1), 0, i*12+2};
+  cfg.mapping((i-1)*12+4,  :) = {sprintf('%d_Eb', i-1), 0, i*12+3};
+  cfg.mapping((i-1)*12+5,  :) = {sprintf('%d_E' , i-1), 0, i*12+4};
+  cfg.mapping((i-1)*12+6,  :) = {sprintf('%d_F' , i-1), 0, i*12+5};
+  cfg.mapping((i-1)*12+7,  :) = {sprintf('%d_Gb', i-1), 0, i*12+6};
+  cfg.mapping((i-1)*12+8,  :) = {sprintf('%d_G' , i-1), 0, i*12+7};
+  cfg.mapping((i-1)*12+9,  :) = {sprintf('%d_Ab', i-1), 0, i*12+8};
+  cfg.mapping((i-1)*12+10, :) = {sprintf('%d_A' , i-1), 0, i*12+9};
+  cfg.mapping((i-1)*12+11, :) = {sprintf('%d_Bb', i-1), 0, i*12+10};
+  cfg.mapping((i-1)*12+12, :) = {sprintf('%d_B' , i-1), 0, i*12+11};
 end
 
 close all
-h = figure;
-guidata(h, cfg);
-set(h, 'DeleteFcn', @cb_cleanup);
-creategui(h);
 
 if strcmp(cfg.input, 'yes')
   midiOpen('input');
@@ -70,6 +70,11 @@ end
 if strcmp(cfg.output, 'yes')
   midiOpen('output');
 end
+
+h = figure;
+guidata(h, cfg);
+set(h, 'DeleteFcn', @cb_cleanup);
+creategui(h);
 
 if strcmp(cfg.input, 'yes')
   t = timer('ExecutionMode', 'fixedRate', 'Period', 0.1, 'UserData', h, 'TimerFcn', @cb_timer);
@@ -83,11 +88,29 @@ function creategui(h)
 
 figure(h);
 pos = get(h, 'Position');
-pos(3) = 740;
+pos(1) = pos(1)+pos(3)/2-1080/2;
+pos(3) = 1080;
 pos(4) = 140;
 set(h, 'Position', pos);
 set(h, 'MenuBar', 'none')
 set(h, 'Name', 'Piano')
+
+uicontrol('tag', '0_A',  'style', 'pushbutton', 'string', '');
+uicontrol('tag', '0_B',  'style', 'pushbutton', 'string', '');
+uicontrol('tag', '0_Bb', 'style', 'pushbutton', 'string', '');
+
+uicontrol('tag', '1_C',  'style', 'pushbutton', 'string', '');
+uicontrol('tag', '1_D',  'style', 'pushbutton', 'string', '');
+uicontrol('tag', '1_E',  'style', 'pushbutton', 'string', '');
+uicontrol('tag', '1_F',  'style', 'pushbutton', 'string', '');
+uicontrol('tag', '1_G',  'style', 'pushbutton', 'string', '');
+uicontrol('tag', '1_A',  'style', 'pushbutton', 'string', '');
+uicontrol('tag', '1_B',  'style', 'pushbutton', 'string', '');
+uicontrol('tag', '1_Db', 'style', 'pushbutton', 'string', '');
+uicontrol('tag', '1_Eb', 'style', 'pushbutton', 'string', '');
+uicontrol('tag', '1_Gb', 'style', 'pushbutton', 'string', '');
+uicontrol('tag', '1_Ab', 'style', 'pushbutton', 'string', '');
+uicontrol('tag', '1_Bb', 'style', 'pushbutton', 'string', '');
 
 uicontrol('tag', '2_C',  'style', 'pushbutton', 'string', '');
 uicontrol('tag', '2_D',  'style', 'pushbutton', 'string', '');
@@ -154,6 +177,21 @@ uicontrol('tag', '6_Gb', 'style', 'pushbutton', 'string', '');
 uicontrol('tag', '6_Ab', 'style', 'pushbutton', 'string', '');
 uicontrol('tag', '6_Bb', 'style', 'pushbutton', 'string', '');
 
+uicontrol('tag', '7_C',  'style', 'pushbutton', 'string', '');
+uicontrol('tag', '7_D',  'style', 'pushbutton', 'string', '');
+uicontrol('tag', '7_E',  'style', 'pushbutton', 'string', '');
+uicontrol('tag', '7_F',  'style', 'pushbutton', 'string', '');
+uicontrol('tag', '7_G',  'style', 'pushbutton', 'string', '');
+uicontrol('tag', '7_A',  'style', 'pushbutton', 'string', '');
+uicontrol('tag', '7_B',  'style', 'pushbutton', 'string', '');
+uicontrol('tag', '7_Db', 'style', 'pushbutton', 'string', '');
+uicontrol('tag', '7_Eb', 'style', 'pushbutton', 'string', '');
+uicontrol('tag', '7_Gb', 'style', 'pushbutton', 'string', '');
+uicontrol('tag', '7_Ab', 'style', 'pushbutton', 'string', '');
+uicontrol('tag', '7_Bb', 'style', 'pushbutton', 'string', '');
+
+uicontrol('tag', '8_C',  'style', 'pushbutton', 'string', '');
+
 % all controls have the same callback function
 ft_uilayout(h, 'tag', '^.*$', 'callback', @cb_interface);
 
@@ -180,10 +218,15 @@ ft_uilayout(h, 'tag', 'Gb$', 'hpos', 093);
 ft_uilayout(h, 'tag', 'Ab$', 'hpos', 113);
 ft_uilayout(h, 'tag', 'Bb$', 'hpos', 133);
 
-ft_uilayout(h, 'tag', '^3', 'hshift', 1*140);
-ft_uilayout(h, 'tag', '^4', 'hshift', 2*140);
-ft_uilayout(h, 'tag', '^5', 'hshift', 3*140);
-ft_uilayout(h, 'tag', '^6', 'hshift', 4*140);
+ft_uilayout(h, 'tag', '^0', 'hshift', 40-1*140);
+ft_uilayout(h, 'tag', '^1', 'hshift', 40+0*140);
+ft_uilayout(h, 'tag', '^2', 'hshift', 40+1*140);
+ft_uilayout(h, 'tag', '^3', 'hshift', 40+2*140);
+ft_uilayout(h, 'tag', '^4', 'hshift', 40+3*140);
+ft_uilayout(h, 'tag', '^5', 'hshift', 40+4*140);
+ft_uilayout(h, 'tag', '^6', 'hshift', 40+5*140);
+ft_uilayout(h, 'tag', '^7', 'hshift', 40+6*140);
+ft_uilayout(h, 'tag', '^8', 'hshift', 40+7*140);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % SUBFUNCTION
@@ -198,19 +241,19 @@ octave = str2double(tag(1));
 note   = tag(3:end);
 
 if strcmp(cfg.playsound, 'yes')
-  % I removed the first column to make the indexing consistent with the notation http://en.wikipedia.org/wiki/Scientific_pitch_notation
-  frequency.C  = [32.703 65.406 130.81 261.63 523.25 1046.5 2093.0 4186.0 8372.0 16744.0];
-  frequency.Db = [34.648 69.296 138.59 277.18 554.37 1108.7 2217.5 4434.9 8869.8 17739.7];
-  frequency.D  = [36.708 73.416 146.83 293.66 587.33 1174.7 2349.3 4698.6 9397.3 18794.5];
-  frequency.Eb = [38.891 77.782 155.56 311.13 622.25 1244.5 2489.0 4978.0 9956.1 19912.1];
-  frequency.E  = [41.203 82.407 164.81 329.63 659.26 1318.5 2637.0 5274.0 10548.1 21096.2];
-  frequency.F  = [43.654 87.307 174.61 349.23 698.46 1396.9 2793.8 5587.7 11175.3 22350.6];
-  frequency.Gb = [46.249 92.499 185.00 369.99 739.99 1480.0 2960.0 5919.9 11839.8 23679.6];
-  frequency.G  = [48.999 97.999 196.00 392.00 783.99 1568.0 3136.0 6271.9 12543.9 25087.7];
-  frequency.Ab = [51.913 103.83 207.65 415.30 830.61 1661.2 3322.4 6644.9 13289.8 26579.5];
-  frequency.A  = [55.000 110.00 220.00 440.00 880.00 1760.0 3520.0 7040.0 14080.0 28160.0];
-  frequency.Bb = [58.270 116.54 233.08 466.16 932.33 1864.7 3729.3 7458.6 14917.2 29834.5];
-  frequency.B  = [61.735 123.47 246.94 493.88 987.77 1975.5 3951.1 7902.1 15804.3 31608.5];
+  % the first column is octave 0 according to http://en.wikipedia.org/wiki/Scientific_pitch_notation
+  frequency.C  = [16.352 32.703 65.406 130.81 261.63 523.25 1046.5 2093.0 4186.0  8372.0 16744.0];
+  frequency.Db = [17.324 34.648 69.296 138.59 277.18 554.37 1108.7 2217.5 4434.9  8869.8 17739.7];
+  frequency.D  = [18.354 36.708 73.416 146.83 293.66 587.33 1174.7 2349.3 4698.6  9397.3 18794.5];
+  frequency.Eb = [19.445 38.891 77.782 155.56 311.13 622.25 1244.5 2489.0 4978.0  9956.1 19912.1];
+  frequency.E  = [20.602 41.203 82.407 164.81 329.63 659.26 1318.5 2637.0 5274.0 10548.1 21096.2];
+  frequency.F  = [21.827 43.654 87.307 174.61 349.23 698.46 1396.9 2793.8 5587.7 11175.3 22350.6];
+  frequency.Gb = [23.125 46.249 92.499 185.00 369.99 739.99 1480.0 2960.0 5919.9 11839.8 23679.6];
+  frequency.G  = [24.500 48.999 97.999 196.00 392.00 783.99 1568.0 3136.0 6271.9 12543.9 25087.7];
+  frequency.Ab = [25.957 51.913 103.83 207.65 415.30 830.61 1661.2 3322.4 6644.9 13289.8 26579.5];
+  frequency.A  = [27.500 55.000 110.00 220.00 440.00 880.00 1760.0 3520.0 7040.0 14080.0 28160.0];
+  frequency.Bb = [29.135 58.270 116.54 233.08 466.16 932.33 1864.7 3729.3 7458.6 14917.2 29834.5];
+  frequency.B  = [30.868 61.735 123.47 246.94 493.88 987.77 1975.5 3951.1 7902.1 15804.3 31608.5];
   
   fs = 22000;
   t = (1:0.5*fs)/fs;
@@ -222,21 +265,21 @@ if strcmp(cfg.playsound, 'yes')
 end
 
 if strcmp(cfg.output, 'yes')
-  midi.C  = (1:10)*12 + 12 + 0;
-  midi.Db = (1:10)*12 + 12 + 1;
-  midi.D  = (1:10)*12 + 12 + 2;
-  midi.Eb = (1:10)*12 + 12 + 3;
-  midi.E  = (1:10)*12 + 12 + 4;
-  midi.F  = (1:10)*12 + 12 + 5;
-  midi.Gb = (1:10)*12 + 12 + 6;
-  midi.G  = (1:10)*12 + 12 + 7;
-  midi.Ab = (1:10)*12 + 12 + 8;
-  midi.A  = (1:10)*12 + 12 + 9;
-  midi.Bb = (1:10)*12 + 12 + 10;
-  midi.B  = (1:10)*12 + 12 + 11;
+  midi.C  = (1:11)*12 + 0;
+  midi.Db = (1:11)*12 + 1;
+  midi.D  = (1:11)*12 + 2;
+  midi.Eb = (1:11)*12 + 3;
+  midi.E  = (1:11)*12 + 4;
+  midi.F  = (1:11)*12 + 5;
+  midi.Gb = (1:11)*12 + 6;
+  midi.G  = (1:11)*12 + 7;
+  midi.Ab = (1:11)*12 + 8;
+  midi.A  = (1:11)*12 + 9;
+  midi.Bb = (1:11)*12 + 10;
+  midi.B  = (1:11)*12 + 11;
   
   n = midi.(note);
-  n = n(octave);
+  n = n(octave+1);
   midiOut('+', cfg.channel, n, cfg.velocity)
 end
 
@@ -250,6 +293,10 @@ dat = midiIn('G');
 
 if isempty(dat)
   return
+end
+
+for i=1:size(dat,1)
+  fprintf('input: channel %3d, note %3d, velocity %3d, timestamp %d\n', dat(i,1), dat(i,2), dat(i,3), dat(i,4));
 end
 
 h   = get(t, 'UserData');
@@ -275,8 +322,13 @@ for i=1:size(dat,1)
             % pushed down
             set(u, 'backgroundcolor', [1 0 0]);
           else
-            % released
-            set(u, 'backgroundcolor', [0.9294 0.9294 0.9294]);
+            % released, switch back to white or black
+            tmp = get(u, 'tag');
+            if tmp(end)=='b'
+              set(u, 'backgroundcolor', [0 0 0]);
+            else
+              set(u, 'backgroundcolor', [1 1 1]);
+            end
           end
         case 'popupmenu'
           set(u, 'value', dat(i,3));
