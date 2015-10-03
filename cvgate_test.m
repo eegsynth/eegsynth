4ce2596b9fbb5f3be0fe76e5167db849d9886a71
Robert Oostenveld <r.oostenveld@gmail.com>
2015-10-03 18:02:57 +0200

    implemented GUI to select serial port and to test voltage and gate outputs

--- cvgate_test.m
+++ cvgate_test.m
@@ -1,4 +1,4 @@
-a = cvgate('/dev/tty.wchusbserial410', 115200);
+a = cvgate('/dev/tty.wchusbserial620', 115200);
 
 a.voltage = [];
 a.gate    = [];
@@ -7,9 +7,9 @@ c = 0;
 t = tic;
 while true
   a.voltage(1) = 5 * (0.4 + sin(0.5*2*pi*toc(t))/2);
-  a.voltage(2) = 5 * (0.3 + sin(0.5*2*pi*toc(t))/2);
-  a.voltage(3) = 5 * (0.2 + sin(0.5*2*pi*toc(t))/2);
-  a.voltage(4) = 5 * (0.1 + sin(0.5*2*pi*toc(t))/2);
+%   a.voltage(2) = 5 * (0.3 + sin(0.5*2*pi*toc(t))/2);
+%   a.voltage(3) = 5 * (0.2 + sin(0.5*2*pi*toc(t))/2);
+%   a.voltage(4) = 5 * (0.1 + sin(0.5*2*pi*toc(t))/2);
   
   update(a);
   c = c + 1;
