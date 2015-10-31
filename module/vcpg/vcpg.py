0c2874f55f379d130c966ef3de16102f2779b7b0
Robert Oostenveld <r.oostenveld@gmail.com>
2015-10-31 19:45:24 +0100

    some new python code for Stephen

--- module/vcpg/vcpg.py
+++ module/vcpg/vcpg.py
@@ -1,5 +1,8 @@
-import ConfigParser
+#!/usr/bin/python
+import time
+import ConfigParser # this is version 2.x specific, on version 3.x it is called "configparser" and has a different API
 import redis
+import serial
 
 config = ConfigParser.ConfigParser()
 config.read('vcpg.ini')
@@ -13,3 +16,16 @@ for section in config.sections():
 r = redis.StrictRedis(host=config.get('input','hostname'), port=int(config.get('input','port')), db=0)
 r.set('foo', 'bar')
 r.get('foo')
+
+
+s = serial.Serial(onfig.get('output','device'), int(config.get('output','baudrate')), timeout=3.0)
+
+while True:
+    s.write("\r\nSay something:")
+    rcv = s.read(10)
+    s.write("\r\nYou sent:" + repr(rcv))
+
+
+print "Start : %s" % time.ctime()
+time.sleep( 5 )
+print "End : %s" % time.ctime()
