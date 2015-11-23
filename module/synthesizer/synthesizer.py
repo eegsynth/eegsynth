f6cdd95bbfe20371d808370cd6c21e6ad0ca09b6
Robert Oostenveld <r.oostenveld@gmail.com>
2015-11-23 22:16:38 +0100

    implemented simple VCA, single knob can be used to control the amplitude

--- module/synthesizer/synthesizer.py
+++ module/synthesizer/synthesizer.py
@@ -2,65 +2,97 @@
 
 import math
 import pyaudio
-import ConfigParser # this is version 2.x specific, on version 3.x it is called "configparser" and has a different API
+import ConfigParser # this is version 2.x specific,on version 3.x it is called "configparser" and has a different API
 import redis
 
 config = ConfigParser.ConfigParser()
 config.read('synthesizer.ini')
 
-r = redis.StrictRedis(host=config.get('redis','hostname'), port=config.getint('redis','port'), db=0)
+r = redis.StrictRedis(host=config.get('redis','hostname'),port=config.getint('redis','port'),db=0)
 p = pyaudio.PyAudio()
 
-BITRATE   = int(config.get('general', 'bitrate'))
-BLOCKSIZE = int(config.get('general', 'blocksize'))
-FREQUENCY = 256.0
+BITRATE   = int(config.get('general','bitrate'))
+BLOCKSIZE = int(config.get('general','blocksize'))
+CHANNELS  = 1
 BITS = p.get_format_from_width(1)
-CHANNELS = 1
 
-stream = p.open(format = BITS, channels = CHANNELS, rate = BITRATE, output = True)
+stream = p.open(format = BITS,channels = CHANNELS,rate = BITRATE,output = True)
 
 offset = 0
 while True:
 
-  ampl_sin = r.get(config.get('input', 'ampl_sin'))
-  if ampl_sin:
-    ampl_sin = float(ampl_sin)
-  else:
-    ampl_sin = config.getfloat('default','ampl_sin')
+  ################################################################################
+  # VCO
+  ################################################################################
 
-  ampl_sqr = r.get(config.get('input', 'ampl_sqr'))
-  if ampl_sqr:
-    ampl_sqr = float(ampl_sqr)
+  pitch = r.get(config.get('input','pitch'))
+  if pitch:
+    pitch = float(pitch)
   else:
-    ampl_sqr = config.getfloat('default','ampl_sqr')
+    pitch = config.getfloat('default','pitch')
 
-  ampl_tri = r.get(config.get('input', 'ampl_tri'))
-  if ampl_tri:
-    ampl_tri = float(ampl_tri)
+  if config.get('general','taper')=='linear':
+    # assume that this value is between 0 and 127
+    # map it to a comfortable frequency range
+    FREQUENCY = 10*pitch + 1
   else:
-    ampl_tri = config.getfloat('default','ampl_tri')
+    FREQUENCY = math.exp(pitch);
 
-  ampl_saw = r.get(config.get('input', 'ampl_saw'))
-  if ampl_saw:
-    ampl_saw = float(ampl_saw)
+  control_sin = r.get(config.get('input','sin'))
+  if control_sin:
+    control_sin = float(control_sin)
   else:
-    ampl_saw = config.getfloat('default','ampl_saw')
+    control_sin = config.getfloat('default','sin')
 
-  ampl_total = ampl_sin + ampl_sqr + ampl_tri + ampl_saw
-  if ampl_total>0:
-      ampl_sin = ampl_sin/ampl_total
-      ampl_sqr = ampl_sqr/ampl_total
-      ampl_tri = ampl_tri/ampl_total
-      ampl_saw = ampl_saw/ampl_total
+  control_tri = r.get(config.get('input','tri'))
+  if control_tri:
+    control_tri = float(control_tri)
+  else:
+    control_tri = config.getfloat('default','tri')
+
+  control_saw = r.get(config.get('input','saw'))
+  if control_saw:
+    control_saw = float(control_saw)
+  else:
+    control_saw = config.getfloat('default','saw')
+
+  control_sqr = r.get(config.get('input','sqr'))
+  if control_sqr:
+    control_sqr = float(control_sqr)
+  else:
+    control_sqr = config.getfloat('default','sqr')
+
+  control_total = control_sin + control_tri + control_saw + control_sqr
+  if control_total>0:
+      # these are all scaled relatively to each other
+      control_sin = control_sin/control_total
+      control_tri = control_tri/control_total
+      control_saw = control_saw/control_total
+      control_sqr = control_sqr/control_total
+
+  ################################################################################
+  # VCA
+  ################################################################################
+
+  control_vca = r.get(config.get('input','vca'))
+  if control_sqr:
+    control_vca = float(control_vca)
+  else:
+    control_vca = config.getfloat('default','vca')
+  # assume that this value is between 0 and 127
+  control_vca = control_vca/127.0
 
   BUFFER = ''
   PERIOD = int(BITRATE/FREQUENCY)
-  for t in xrange(offset, offset+BLOCKSIZE):
-    wave_sin = ampl_sin * (math.sin(math.pi*FREQUENCY*t/BITRATE)+1)/2
-    wave_sqr = ampl_sqr * float((t % PERIOD) > PERIOD/2)
-    wave_tri = ampl_tri * float(abs(t % PERIOD - PERIOD/2))/PERIOD*2
-    wave_saw = ampl_saw * float(t % PERIOD)/PERIOD
-    waveform = (wave_sin + wave_sqr + wave_tri + wave_saw)*255
+  for t in xrange(offset,offset+BLOCKSIZE):
+    # compose the VCO
+    wave_sin = control_sin * (math.sin(math.pi*FREQUENCY*t/BITRATE)+1)/2
+    wave_tri = control_tri * float(abs(t % PERIOD - PERIOD/2))/PERIOD*2
+    wave_saw = control_saw * float(t % PERIOD)/PERIOD
+    wave_sqr = control_sqr * float((t % PERIOD) > PERIOD/2)
+    waveform = (wave_sin + wave_tri + wave_saw + wave_sqr)*127
+    # apply the VCA
+    waveform = control_vca * waveform
     BUFFER = BUFFER+chr(int(waveform))
   offset = offset+BLOCKSIZE
   stream.write(BUFFER)
