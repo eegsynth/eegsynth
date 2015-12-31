b3703fcb990a0e3c2e34e24e6f28fb7778237544
Robert Oostenveld <r.oostenveld@gmail.com>
2015-12-31 10:10:56 +0100

    ENH added start/stop/restart/status bash script for all modules, so far only actually working for buffer

---
+++ module/devirtualizer/devirtualizer.sh
@@ -0,0 +1,65 @@
+#! /bin/sh
+
+PATH=/sbin:/bin:/usr/bin
+
+COMMAND="sleep 600"
+
+DIR=`dirname $0`
+NAME=`basename $0 .sh`
+
+# helper files are stored in the directory containing this script
+PIDFILE=${DIR}/${NAME}.pid
+LOGFILE=${DIR}/${NAME}.log
+
+log_action_msg () {
+  echo $* 1>&2
+}
+
+check_running_process () {
+  if [ ! -f "$PIDFILE" ]; then
+    return 1
+  else
+    kill -0 `cat $PIDFILE` 2> /dev/null
+    return $?
+  fi
+}
+
+do_start () {
+  log_action_msg "Starting $NAME"
+  check_running_process && log_action_msg "Error: $NAME is already started" && exit 1
+  # start the process in the background
+  ( $COMMAND > $LOGFILE) &
+  echo $! > $PIDFILE
+}
+
+do_stop () {
+  log_action_msg "Stopping $NAME"
+  check_running_process || log_action_msg "Error: $NAME is already stopped"
+  check_running_process || exit 1
+  kill -9 `cat $PIDFILE`
+  rm $PIDFILE
+}
+
+do_status () {
+  check_running_process && YESNO=" " || YESNO=" not "
+  log_action_msg "$NAME is${YESNO}running"
+}
+
+case "$1" in
+  start)
+        do_start
+        ;;
+  restart)
+        do_stop && do_start
+        ;;
+  stop)
+        do_stop
+        ;;
+  status)
+        do_status
+        ;;
+  *)
+        echo "Usage: $0 start|stop|restart|status" >&2
+        exit 3
+        ;;
+esac
