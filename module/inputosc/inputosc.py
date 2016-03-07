dd3f0be4c39552a6a5c272f3e259e625d91ee6d6
Robert Oostenveld <r.oostenveld@gmail.com>
2016-03-07 23:09:31 +0100

    ENH - implemented inputosc module, which reads from OSC and writes to REDIS

--- module/inputosc/inputosc.py
+++ module/inputosc/inputosc.py
@@ -3,17 +3,18 @@
 import sys
 import os
 import socket
-import re
 import time
 import threading
-import ConfigParser # this is version 2.x specific, on version 3.x it is called "configparser" and has a different API
 import redis
+import ConfigParser # this is version 2.x specific, on version 3.x it is called "configparser" and has a different API
 import OSC          # see https://trac.v2.nl/wiki/pyOSC
 
 if hasattr(sys, 'frozen'):
     basis = sys.executable
-else:
+elif sys.argv[0]!='':
     basis = sys.argv[0]
+else:
+    basis = './'
 installed_folder = os.path.split(basis)[0]
 
 config = ConfigParser.ConfigParser()
@@ -29,38 +30,45 @@ except redis.ConnectionError:
 s = OSC.OSCServer((config.get('osc','address'), config.getint('osc','port')))
 
 # define a message-handler function for the server to call.
-def test_handler(addr, tags, stuff, source):
-	print "---"
-	print "received new osc msg from %s" % OSC.getUrlStr(source)
-	print "with addr : %s" % addr
-	print "typetags %s" % tags
-	print "data %s" % stuff
-	print "---"
+def forward_handler(addr, tags, data, source):
+    print "---"
+    print "source %s" % OSC.getUrlStr(source)
+    print "addr   %s" % addr
+    print "tags   %s" % tags
+    print "data   %s" % data
+    # prefix.addr=value
+    key = config.get('output', 'prefix')+addr.replace('/', '.')
+    if tags=='f' or tags=='i':
+        # send it as a single value
+        val = data[0]
+        r.set(key, val)
+    else:
+        # send it as a list
+        val = data
+        r.set(key, val)
 
-s.noCallback_handler = test_handler
-
-# adding my functions
+s.noCallback_handler = forward_handler
 s.addDefaultHandlers()
-s.addMsgHandler("/1/faderD", test_handler)
+# s.addMsgHandler("/1/faderD", test_handler)
 
 # just checking which handlers we have added
 print "Registered Callback-functions are :"
 for addr in s.getOSCAddressSpace():
 	print addr
 
-# Start OSCServer
-print "\nStarting OSCServer. Use ctrl-C to quit."
+# start the server thread
+print "\nStarting module. Use ctrl-C to quit."
 st = threading.Thread( target = s.serve_forever )
 st.start()
 
-# Loop while threads are running.
-try :
-	while 1 :
+# loop while threads are running
+try:
+	while True:
 		time.sleep(10)
 
-except KeyboardInterrupt :
-	print "\nClosing OSCServer."
+except KeyboardInterrupt:
+	print "\nClosing module."
 	s.close()
-	print "Waiting for Server-thread to finish"
+	print "Waiting for Server-thread to finish."
 	st.join()
-	print "Done"
+	print "Done."
