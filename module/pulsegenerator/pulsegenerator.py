39422c497e41e4dceb13698da4335f1b755cb304
Robert Oostenveld <r.oostenveld@gmail.com>
2016-03-25 14:47:26 +0100

    revamped the pulsegenerator module, it now allows for synchronization with redis, midi and serial. All three can be enabled/disabled on the fly. Also added is a time-slip correction to keep them better in sync.

--- module/pulsegenerator/pulsegenerator.py
+++ module/pulsegenerator/pulsegenerator.py
@@ -1,17 +1,26 @@
 #!/usr/bin/env python
 
-import time
 import ConfigParser # this is version 2.x specific, on version 3.x it is called "configparser" and has a different API
 import redis
+import mido
 # import serial
+import threading
+import time
 import sys
 import os
+import numpy as np
 
 if hasattr(sys, 'frozen'):
-    basis = sys.executable
-else:
+    basis = syserialport.executable
+elif sys.argv[0]!='':
     basis = sys.argv[0]
+else:
+    basis = './'
 installed_folder = os.path.split(basis)[0]
+
+# eegsynth/lib contains shared modules
+sys.path.insert(0, os.path.join(installed_folder,'../../lib'))
+import EEGsynth
 
 config = ConfigParser.ConfigParser()
 config.read(os.path.join(installed_folder, 'pulsegenerator.ini'))
@@ -26,59 +35,208 @@ except redis.ConnectionError:
     print "Error: cannot connect to redis server"
     exit()
 
-# send it to redis rather than sending it directly to the serial interface
-# s = serial.Serial(config.get('serial','device'), config.getint('serial','baudrate'), timeout=3.0)
+serialport = None
+def initialize_serial():
+    try:
+        serialport = serial.Serial(config.get('serial','device'), config.getint('serial','baudrate'), timeout=3.0)
+    except:
+        print "Error: cannot connect to serial output"
+        exit()
+    return serialport
 
-previous_offset=0
-delay=60
+midiport = None
+def initialize_midi():
+    # this is only for debugging
+    print('------ OUTPUT ------')
+    for port in mido.get_output_names():
+      print(port)
+    print('-------------------------')
+
+    mididevice  = config.get('midi', 'device')
+    try:
+        midiport  = mido.open_output(mididevice)
+        if debug>0:
+            print "Connected to MIDI output"
+    except:
+        print "Error: cannot connect to MIDI output"
+        exit()
+    return midiport
+
+# this is to prevent two messages from being sent at the same time
+lock = threading.Lock()
+
+class TriggerThread(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.running = True
+        self.enabled = False
+        self.rate = 60
+    def setRate(self, rate):
+        lock.acquire()
+        self.rate = rate
+        lock.release()
+    def enable(self):
+        self.enabled = True
+    def disable(self):
+        self.enabled = False
+    def stop(self):
+        self.running = False
+    def run(self):
+        msg = mido.Message('clock')
+        slip = 0
+        if debug>0:
+            print msg
+        while self.running:
+            if not self.enabled:
+                time.sleep(0.1)
+            else:
+                now = time.time()
+                delay = 60/self.rate      # the rate is in bpm
+                delay -= slip             # correct for the slip from the previous iteration
+                jiffy = delay/(24)
+                for iteration in range(24):
+                    midiport.send(msg)
+                    time.sleep(jiffy)
+                # the time used in this loop will be slightly different than desired
+                slip = time.time() - now - delay
+
+def find_nearest_idx(array,value):
+    # find the index of the array element that is the nearest to the value
+    idx = (np.abs(np.asarray(array)-value)).argmin()
+    return idx
+
+multiplier_mid = [   10,    30,    50,   70,   90,   110,   130 ] # lookup table
+multiplier_val = [ 1./4,  1./3,  1./1,    1,    2,     3,     4 ] # actual value
+
+slip            = 0
+tick            = 0
+adjust_offset   = 0
+previous_offset = 0
 pulselength = config.getfloat('general','pulselength')
 key=config.get('output','channel')
 
-while True:
-    val = r.get(config.get('input', 'channel'))
-    if val:
-        cv_rate = float(val)
-    else:
-        cv_rate = config.getfloat('default','rate')
+# these will only be started when needed
+init_midi   = False
+init_serial = False
 
-    val = r.get(config.get('input', 'offset'))
-    if val:
-        cv_offset = float(val)
-    else:
-        cv_offset = config.getfloat('default','offset')
+previous_use_midi = None
+previous_use_serial = None
 
-    val = r.get(config.get('input', 'multiplier'))
-    if val:
-        cv_multiplier = float(val)
-    else:
-        cv_multiplier = config.getfloat('default','multiplier')
+try:
+    # start the thread that synchronizes over MIDI
+    midisync = TriggerThread()
+    midisync.start()
 
-    adjust_offset = previous_offset - cv_offset
-    try:
-    	delay = 60/(cv_rate*cv_multiplier) - adjust_offset
-    except:
-        pass
+    while True:
+        # measure the time to correct for the slip
+        now = time.time()
 
-    print 'delay in seconds =', delay
+        use_serial = EEGsynth.getint('general', 'serial', config, r)>0 or False
+        use_midi   = EEGsynth.getint('general', 'midi', config, r)>0 or False
+        use_redis  = EEGsynth.getint('general', 'redis', config, r)>0 or False
 
-    # send it to redis rather than sending it directly to the serial interface
-    # s.write('*g1v1#')
-    # s.write('*g2v1#')
-    val = 127               # set the note on
-    r.publish(key,val)      # send it as trigger
-    if pulselength<(delay/2):
-        time.sleep(pulselength)
-    else:
-        # the desired duration between beats is too short, split the delay in half
-        time.sleep(delay/2)
+        if previous_use_serial is None:
+            previous_use_serial = not(use_serial);
 
-    # send it to redis rather than sending it directly to the serial interface
-    # s.write('*g1v0#')
-    # s.write('*g2v0#')
-    val = 0                 # set the note off
-    r.publish(key,val)      # send it as trigger
-    if pulselength<(delay/2):
-        time.sleep(delay-pulselength)
-    else:
-        # the desired duration between beats is too short, split the delay in half
-        time.sleep(delay/2)
+        if previous_use_midi is None:
+            previous_use_midi = not(use_midi);
+
+        print "use", use_serial, use_midi, use_redis
+
+        if use_serial and not init_serial:
+            # this might not be running at the start
+            s = initialize_serial()
+            init_serial = True
+
+        if use_midi and not init_midi:
+            # this might not be running at the start
+            midiport = initialize_midi()
+            init_midi = True
+
+        if use_midi and not previous_use_midi:
+            midisync.enable()
+            previous_use_midi = True
+        elif not use_midi and previous_use_midi:
+            midisync.disable()
+            previous_use_midi = False
+
+        rate = EEGsynth.getfloat('input', 'rate', config, r)
+        if rate is None:
+            time.sleep(0.1)
+            continue
+
+        offset = EEGsynth.getfloat('input', 'offset', config, r)
+        # the offset value from 0-127 gets scaled to a value from -1 to +1 seconds
+        if offset is None:
+            offset = 0
+        offset = (offset-64)/(127/2)
+
+        multiplier = EEGsynth.getfloat('input', 'multiplier', config, r)
+        # the multiplier value from 0-127 gets scaled to a value from 1/4 to 16/4
+        idx = find_nearest_idx(multiplier_mid, multiplier)
+        multiplier = multiplier_val[idx]
+
+        # update the thread that synchronizes over MIDI
+        midisync.setRate(rate*multiplier)
+
+        try:
+        	delay = 60/(rate*multiplier)
+        except:
+            delay = 1
+        delay = max(delay, pulselength) # cannot be shorter than the pulse length
+
+        adjust_offset  += offset - previous_offset  # correct for the user specified offset
+        previous_offset = offset
+
+        if debug>0:
+            print rate, offset, multiplier, adjust_offset, slip
+
+        if (adjust_offset-slip)<0:
+            if (delay-pulselength+adjust_offset-slip)<0:
+                if debug>0:
+                    print "skip"
+                # don't send a pulse and don't sleep at all
+                time.sleep(0)
+                adjust_offset += (delay-pulselength)
+            else:
+                if debug>0:
+                    print "tick", tick
+                    tick+=1
+                if use_serial:
+                    serialport.write('*g2v1#')
+                if use_redis:
+                    r.publish(key,127)      # send it as trigger
+                time.sleep(pulselength)
+                # sleep a reduced amount
+                time.sleep(delay-pulselength+adjust_offset-slip)
+                adjust_offset = 0
+                if use_serial:
+                    serialport.write('*g2v0#')
+                if use_redis:
+                    r.publish(key,0)        # send it as trigger
+        else:
+            if debug>0:
+                print "tick", tick
+                tick+=1
+            if use_serial:
+                serialport.write('*g2v1#')
+            if use_redis:
+                r.publish(key,127)          # send it as trigger
+            time.sleep(pulselength)
+            # sleep an increased amount
+            time.sleep(delay-pulselength+adjust_offset-slip)
+            adjust_offset = 0
+            if use_serial:
+                serialport.write('*g2v0#')
+            if use_redis:
+                r.publish(key,0)            # send it as trigger
+
+        # the time used in this loop will be slightly different than desired
+        # this will be corrected in the next iteration
+        slip += time.time() - now - delay
+
+except KeyboardInterrupt:
+    print "Closing threads"
+    midisync.stop()
+    midisync.join()
+    sys.exit()
