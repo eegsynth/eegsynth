1cf7aacf37fcafb859cd5c3f5accef66de620341
Robert Oostenveld <r.oostenveld@gmail.com>
2016-10-09 12:20:21 +0200

    added jaga2ft module for Jinga-Hi JAGA16 WiFi EEG system

--- bin/install.sh
+++ bin/install.sh
@@ -5,22 +5,22 @@ OS=`uname -s`
 MACHINE=`uname -m`
 
 if [ $OS = Linux ] ; then
-  if [ $MACHINE = armv7l ] ; then
-    # Rasperry Pi version 2 or 2+
-    ARCH=raspberrypi
-  elif [ $MACHINE = armv6l ] ; then
-    # Rasperry Pi version 1
-    ARCH=raspberrypi
+  if [ $MACHINE = i386 ] ; then
+    ARCH=glnx86
   elif [ $MACHINE = x86_64 ] ; then
     ARCH=glnxa64
-  elif [ $MACHINE = i386 ] ; then
-    ARCH=glnx86
+  elif [ $MACHINE = armv6l ] ; then
+    ARCH=raspberrypi1
+  elif [ $MACHINE = armv7l ] ; then
+    ARCH=raspberrypi2
+  elif [ $MACHINE = armv8l ] ; then
+    ARCH=raspberrypi3
   fi
 elif [ $OS = Darwin ] ; then
-  if [ $MACHINE = x86_64 ] ; then
-    ARCH=maci64
-  elif [ $MACHINE = i386 ] ; then
+  if [ $MACHINE = i386 ] ; then
     ARCH=maci
+  elif [ $MACHINE = x86_64 ] ; then
+    ARCH=maci64
   fi
 fi
 
@@ -31,7 +31,7 @@ else
   echo Downloading $ARCH binaries for $OS on $MACHINE
 fi
 
-for NAME in buffer openbci2ft ; do
+for NAME in buffer openbci2ft jaga2ft ; do
   URL=https://github.com/fieldtrip/fieldtrip/raw/master/realtime/bin/$ARCH/$NAME
   echo ------------------------------------------------------------------------------
   echo Downloading binary from $URL
---
+++ module/jaga2ft/README.md
@@ -0,0 +1,9 @@
+Jaga2ft  module
+===============
+
+The goal of this module is to read data from the JAGA16 EEG module (over WiFi) and to write this data to the FieldTrip buffer. At the start of data acquisition the FieldTrip buffer will be reset.
+
+## Requirements
+
+The FieldTrip buffer should be running.
+The computer should be connected to the same WiFi router as the JAGA16.
