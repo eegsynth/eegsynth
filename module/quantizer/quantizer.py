76a86c6f4b3a6de3ddd42f03b4364e0d7f62ca1c
Robert Oostenveld <r.oostenveld@gmail.com>
2017-12-29 20:58:19 +0100

    FIX #173 - implemented scaling of input and output values, improve feedback, improve documentation and example in ini file

--- module/quantizer/quantizer.py
+++ module/quantizer/quantizer.py
@@ -60,20 +60,31 @@ patch = EEGsynth.patch(config, r)
 # this determines how much debugging information gets printed
 debug = patch.getint('general','debug')
 
+# the input scale and offset are used to map Redis values to internal values
+input_scale  = patch.getfloat('input', 'scale', default=127)
+input_offset = patch.getfloat('input', 'offset', default=0)
+# the output scale and offset are used to map internal values to Redis values
+output_scale  = patch.getfloat('output', 'scale', default=0.00787401574803149606)
+output_offset = patch.getfloat('output', 'offset', default=0)
+
 # get the input and output options
 input_channel, input_name = map(list, zip(*config.items('input')))
-quantized_name, quantized_value = map(list, zip(*config.items('quantization')))
+output_name, output_value = map(list, zip(*config.items('quantization')))
 
-# get the list of values for each of the quantization levels
-for i,name in enumerate(quantized_name):
-    quantized_value[i] = patch.getfloat('quantization', name, multiple=True)
+# remove the scale and offset from the input list
+del input_channel[input_channel.index('scale')]
+del input_channel[input_channel.index('offset')]
 
-# find the level to which the data is to be quantized
-index =  quantized_name.index('level')
-level =  quantized_value[index]
-# remove the level from the list
-del quantized_name[index]
-del quantized_value[index]
+# get the list of values for each of the input values for quantization
+for i,name in enumerate(output_name):
+    output_value[i] = patch.getfloat('quantization', name, multiple=True)
+
+# find the input value to which the data is to be quantized
+index       =  output_name.index('value')
+input_value =  output_value[index]
+# remove the value from the output list
+del output_name[index]
+del output_value[index]
 
 def find_nearest_idx(array,value):
     # find the index of the array element that is the nearest to the value
@@ -83,15 +94,27 @@ def find_nearest_idx(array,value):
 while True:
     time.sleep(patch.getfloat('general', 'delay'))
 
-    for channel,name in zip(input_channel, input_name):
+    if debug>0:
+        print '----------------------------------------'
+    for channel, name in zip(input_channel, input_name):
         val = patch.getfloat('input', channel)
         if val is None:
+            if debug>0:
+                print name, 'not found'
             pass
         else:
-            idx = find_nearest_idx(level, val)
-            for qname, qvalue in zip(quantized_name, quantized_value):
+            # map the Redis values to the internally used values
+            val = EEGsynth.rescale(val, slope=input_scale, offset=input_offset)
+            # look up the nearest index of the input_value vector
+            if debug>0:
+                print name, '=', val
+            idx = find_nearest_idx(input_value, val)
+            for qname, qvalue in zip(output_name, output_value):
                 key = '{}.{}'.format(name, qname)
+                # look up the corresponding output value
                 val = qvalue[idx]
-                if debug>1:
+                if debug>0:
                     print key, '=', val
+                # map the internally used values to Redis values
+                val = EEGsynth.rescale(val, slope=output_scale, offset=output_offset)
                 r.set(key, val)             # send it as control value
