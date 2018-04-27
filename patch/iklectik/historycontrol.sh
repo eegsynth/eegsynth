8450a233fa093f566e658face72a7024a1aec132
Robert Oostenveld <r.oostenveld@gmail.com>
2018-04-27 16:50:53 +0100

    restored files, these should not have been deleted

--- patch/iklectik/historycontrol.sh
+++ patch/iklectik/historycontrol.sh
@@ -5,8 +5,7 @@ export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:/usr/local/lib
 
 MODULE=`basename $0 .sh`
 
-EEGSYNTH=../../
+EEGSYNTH=/Users/roboos/eegsynth
 INIDIR=`dirname $0`
 
 $EEGSYNTH/module/$MODULE/$MODULE.py -i $INIDIR/$MODULE.ini
-
