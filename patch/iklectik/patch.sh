6d84afaaf7599e05443061bda69510b5c0a942f0
Robert Oostenveld <r.oostenveld@gmail.com>
2018-04-27 16:51:29 +0100

    updated scripts and ini's, improved startup procedure for OSX

--- patch/iklectik/patch.sh
+++ patch/iklectik/patch.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
-clear
-echo "starting shell script"
 
-mate-terminal \
---tab --title="LaunchControl"   -e "bash -c 'python ../module/launchcontrol/launchcontrol.py      -i launchcontrol.ini';bash" \
---tab --title="Spectral"        -e "bash -c 'python ../module/spectral/spectral.py                -i spectral.ini';bash" \
---tab --title="HistoryControl"  -e "bash -c 'python ../module/historycontrol/historycontrol.py    -i historycontrol.ini'; bash" \
---tab --title="Postprocessing"  -e "bash -c 'python ../module/postprocessing/postprocessing.py    -i postprocessing.ini'; bash" \
---tab --title="Endorphines"     -e "bash -c 'python ../module/endorphines/endorphines.py          -i endorphines.ini';    bash" \
---tab --title="PlotControl"     -e "bash -c 'python ../module/plotcontrol/plotcontrol.py          -i plotcontrol.ini';    bash" \
---tab --title="Plotspectral"    -e "bash -c 'python ../module/plotspectral/plotspectral.py        -i plotspectral.ini';    bash" \
---tab --title="Plotsignal"      -e "bash -c 'python ../module/plotsignal/plotsignal.py            -i plotsignal.ini';    bash" \
---tab --title="OSC"             -e "bash -c 'python ../module/outputosc/outputosc.py              -i outputosc.ini';    bash" \
---tab --title="keyboard"        -e "bash -c 'python ../module/keyboard/keyboard.py                -i keyboard.ini';    bash" \
---tab --title="synchronization" -e "bash -c 'python ../module/synchronization/synchronization.py  -i synchronization.ini';    bash"
+PATCHDIR=`pwd`
 
-# --tab --title="Playbacksignal"   -e "bash -c 'python ../module/playbacksignal/playbacksignal.py     -i playbacksignal.ini';bash" \
+$PATCHDIR/terminal.scpt $PATCHDIR/redis.sh
+$PATCHDIR/terminal.scpt $PATCHDIR/buffer.sh
+$PATCHDIR/terminal.scpt $PATCHDIR/openbci2ft.sh
+$PATCHDIR/terminal.scpt $PATCHDIR/launchcontrol.sh
+$PATCHDIR/terminal.scpt $PATCHDIR/spectral.sh
+$PATCHDIR/terminal.scpt $PATCHDIR/postprocessing.sh
+$PATCHDIR/terminal.scpt $PATCHDIR/historycontrol.sh
+$PATCHDIR/terminal.scpt $PATCHDIR/plotcontrol.sh
+$PATCHDIR/terminal.scpt $PATCHDIR/plotsignal.sh
+$PATCHDIR/terminal.scpt $PATCHDIR/plotspectral.sh
+$PATCHDIR/terminal.scpt $PATCHDIR/outputcvgate.sh
--- patch/iklectik/redis.sh
+++ patch/iklectik/redis.sh
@@ -2,8 +2,7 @@
 
 MODULE=`basename $0 .sh`
 
-EEGSYNTH=../../
+EEGSYNTH=/Users/roboos/eegsynth
 INIDIR=`dirname $0`
 
 $EEGSYNTH/module/$MODULE/$MODULE.sh -i $INIDIR/$MODULE.ini
-
