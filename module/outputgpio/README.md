a68e20a18d7174be5c659ac63741c24512be4935
Robert Oostenveld <r.oostenveld@gmail.com>
2018-06-03 16:58:18 +0200

    added support for user-specified duration, used to switch triggers off

--- module/outputgpio/README.md
+++ module/outputgpio/README.md
@@ -1,8 +1,41 @@
 # Output GPIO module
 
-The purpose of this module is to transform control signals and published events from Redis to the GPIO pins of a Raspberry Pi.
+The purpose of this module is to transform control signals and published events
+from Redis to the GPIO pins of a Raspberry Pi.
+
+The naming of GPIO pins follows the WiringPi convention, which differs from the
+Broadcom and the header pin numbers.
+
+For the Raspberry Pi 2, the mapping between Broadcom, WiringPi and header pins
+is as follows. Please note that the left side of this table corresponds to the
+odd pins of the header and the right side of the table to the even pins.
+
+| BCM | wPi |   Name  | Header | Header | Name    | wPi | BCM |
+|-----|-----|---------|--------|--------|---------|-----|-----|
+|     |     |    3.3v |    1   |   2    | 5v      |     |     |
+|   2 |   8 |   SDA.1 |    3   |   4    | 5v      |     |     |
+|   3 |   9 |   SCL.1 |    5   |   6    | 0v      |     |     |
+|   4 |   7 | GPIO. 7 |    7   |   8    | TxD     | 15  | 14  |
+|     |     |      0v |    9   |   10   | RxD     | 16  | 15  |
+|  17 |   0 | GPIO. 0 |   11   |   12   | GPIO. 1 | 1   | 18  |
+|  27 |   2 | GPIO. 2 |   13   |   14   | 0v      |     |     |
+|  22 |   3 | GPIO. 3 |   15   |   16   | GPIO. 4 | 4   | 23  |
+|     |     |    3.3v |   17   |   18   | GPIO. 5 | 5   | 24  |
+|  10 |  12 |    MOSI |   19   |   20   | 0v      |     |     |
+|   9 |  13 |    MISO |   21   |   22   | GPIO. 6 | 6   | 25  |
+|  11 |  14 |    SCLK |   23   |   24   | CE0     | 10  | 8   |
+|     |     |      0v |   25   |   26   | CE1     | 11  | 7   |
+|   0 |  30 |   SDA.0 |   27   |   28   | SCL.0   | 31  | 1   |
+|   5 |  21 | GPIO.21 |   29   |   30   | 0v      |     |     |
+|   6 |  22 | GPIO.22 |   31   |   32   | GPIO.26 | 26  | 12  |
+|  13 |  23 | GPIO.23 |   33   |   34   | 0v      |     |     |
+|  19 |  24 | GPIO.24 |   35   |   36   | GPIO.27 | 27  | 16  |
+|  26 |  25 | GPIO.25 |   37   |   38   | GPIO.28 | 28  | 20  |
+|     |     |      0v |   39   |   40   | GPIO.29 | 29  | 21  |
+
 
 # Requirements
 
 Redis should be running.
-This module should be started on a Raspberry Pi. 
+
+This module should be started on a Raspberry Pi and [WiringPi](http://wiringpi.com/download-and-install/) should be installed.
--- module/outputgpio/outputgpio.py
+++ module/outputgpio/outputgpio.py
@@ -17,7 +17,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import ConfigParser # this is version 2.x specific, on version 3.x it is called "configparser" and has a different API
+# this is version 2.x specific, on version 3.x it is called "configparser" and has a different API
+import ConfigParser
 import argparse
 import os
 import redis
@@ -28,14 +29,14 @@ import wiringpi
 
 if hasattr(sys, 'frozen'):
     basis = sys.executable
-elif sys.argv[0]!='':
+elif sys.argv[0] != '':
     basis = sys.argv[0]
 else:
     basis = './'
 installed_folder = os.path.split(basis)[0]
 
 # eegsynth/lib contains shared modules
-sys.path.insert(0, os.path.join(installed_folder,'../../lib'))
+sys.path.insert(0, os.path.join(installed_folder, '../../lib'))
 import EEGsynth
 import EDF
 
@@ -47,7 +48,7 @@ config = ConfigParser.ConfigParser()
 config.read(args.inifile)
 
 try:
-    r = redis.StrictRedis(host=config.get('redis','hostname'), port=config.getint('redis','port'), db=0)
+    r = redis.StrictRedis(host=config.get('redis', 'hostname'), port=config.getint('redis', 'port'), db=0)
     response = r.client_list()
 except redis.ConnectionError:
     print "Error: cannot connect to redis server"
@@ -58,89 +59,139 @@ patch = EEGsynth.patch(config, r)
 
 # make a dictionary that maps GPIOs to the WiringPi number
 pin = {
-   "gpio0": 0,
-   "gpio1": 1,
-   "gpio2": 2,
-   "gpio3": 3,
-   "gpio4": 4,
-   "gpio5": 5,
-   "gpio6": 6,
-   "gpio7": 7,
-   "gpio21": 21,
-   "gpio22": 22,
-   "gpio23": 23,
-   "gpio24": 24,
-   "gpio25": 25,
-   "gpio26": 26,
-   "gpio27": 27,
-   "gpio28": 28,
-   "gpio29": 29,
+    "gpio0": 0,
+    "gpio1": 1,
+    "gpio2": 2,
+    "gpio3": 3,
+    "gpio4": 4,
+    "gpio5": 5,
+    "gpio6": 6,
+    "gpio7": 7,
+    "gpio21": 21,
+    "gpio22": 22,
+    "gpio23": 23,
+    "gpio24": 24,
+    "gpio25": 25,
+    "gpio26": 26,
+    "gpio27": 27,
+    "gpio28": 28,
+    "gpio29": 29,
 }
 
-
 # this determines how much debugging information gets printed
-debug        = patch.getint('general','debug')
-delay        = patch.getfloat('general','delay')
-input_scale  = patch.getfloat('input', 'scale', default=100)
+debug = patch.getint('general', 'debug')
+delay = patch.getfloat('general', 'delay')
+# control values for PWM should be between 0 and 100
+input_scale = patch.getfloat('input', 'scale', default=100)
 input_offset = patch.getfloat('input', 'offset', default=0)
+# values between 0 and 1 are quite nice for the duration
+duration_scale = patch.getfloat('duration', 'scale', default=1)
+duration_offset = patch.getfloat('duration', 'offset', default=0)
 
 # this is to prevent two triggers from being activated at the same time
 lock = threading.Lock()
 
+
+def SwitchOn(gpio):
+    lock.acquire()
+    val = 1
+    if debug > 1:
+        print gpio, pin[gpio], val
+    wiringpi.digitalWrite(pin[gpio], val)
+    lock.release()
+
+
+def SwitchOff(gpio):
+    lock.acquire()
+    val = 0
+    if debug > 1:
+        print gpio, pin[gpio], val
+    wiringpi.digitalWrite(pin[gpio], val)
+    lock.release()
+
+
 class TriggerThread(threading.Thread):
-    def __init__(self, redischannel, gpio):
+    def __init__(self, redischannel, gpio, duration):
         threading.Thread.__init__(self)
         self.redischannel = redischannel
         self.gpio = gpio
+        self.duration = duration
         self.running = True
+
     def stop(self):
         self.running = False
+
     def run(self):
         pubsub = r.pubsub()
-        pubsub.subscribe('OUTPUTGPIO_UNBLOCK')  # this message unblocks the Redis listen command
-        pubsub.subscribe(self.redischannel)     # this message triggers the event
+        # this message unblocks the Redis listen command
+        pubsub.subscribe('OUTPUTGPIO_UNBLOCK')
+        # this message triggers the event
+        pubsub.subscribe(self.redischannel)
         while self.running:
             for item in pubsub.listen():
                 if not self.running or not item['type'] == 'message':
                     break
-                print item
-                if item['channel']==self.redischannel:
-                    # the trigger value should also be saved
-                    val = item['data']
-                    val = int(EEGsynth.rescale(val, slope=input_scale, offset=input_offset))
-                    lock.acquire()
-                    wiringpi.digitalWrite(pin[self.gpio], val)
-                    lock.release()
-                    if debug>1:
-                      print self.gpio, pin[self.gpio], val
+                if item['channel'] == self.redischannel:
+                    if self.duration == None:
+                        # switch to the value specified in the event
+                        val = item['data']
+                        val = EEGsynth.rescale(val, slope=input_scale, offset=input_offset)
+                        val = int(val)
+                        if debug > 1:
+                            print self.gpio, pin[self.gpio], val
+                        lock.acquire()
+                        wiringpi.digitalWrite(pin[self.gpio], val)
+                        lock.release()
+                    else:
+                        # switch on and schedule a timer to switch off after the specified duration
+                        SwitchOn(self.gpio)
+                        duration = patch.getfloat('duration', self.gpio)
+                        duration = EEGsynth.rescale(duration, slope=duration_scale, offset=duration_offset)
+                        # some minimal time is needed for the delay
+                        duration = EEGsynth.limit(duration, 0.05, float('Inf'))
+                        t = threading.Timer(duration, SwitchOff, args=[self.gpio])
+                        t.start()
+
 
 # use the WiringPi numbering, see http://wiringpi.com/reference/setup/
 wiringpi.wiringPiSetup()
 
 # set up PWM for the control channels
-for gpio,channel in config.items('control'):
+for gpio, channel in config.items('control'):
     wiringpi.softPwmCreate(pin[gpio], 0, 100)
     print "control", channel, gpio
 
 # create the threads that deal with the triggers
 trigger = []
-for gpio,channel in config.items('trigger'):
+for gpio, channel in config.items('trigger'):
     wiringpi.pinMode(pin[gpio], 1)
-    trigger.append(TriggerThread(channel, gpio))
+    try:
+        duration = patch.getstring('duration', gpio)
+    except:
+        duration = None
+    trigger.append(TriggerThread(channel, gpio, duration))
     print "trigger", channel, gpio
 
 # start the thread for each of the triggers
 for thread in trigger:
     thread.start()
 
+# control values are only relevant when different from the previous value
+previous_val = {}
+for name in control_name:
+    previous_val[name] = None
+
 try:
     while True:
         time.sleep(patch.getfloat('general', 'delay'))
 
-        for gpio,channel in config.items('control'):
+        for gpio, channel in config.items('control'):
             val = patch.getfloat('control', gpio)
-            if val==None:
+            if val == None:
                 continue # it should be skipped when not present
+            if val == previous_val[name]:
+                continue # it should be skipped when identical to the previous value
+            previous_val[name] = val
             val = EEGsynth.rescale(val, slope=input_scale, offset=input_offset)
             val = EEGsynth.limit(val, 0, 100)
             val = int(val)
