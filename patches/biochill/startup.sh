83716c477ed233d1d181e326033d45605395d199
Robert Oostenveld <r.oostenveld@gmail.com>
2018-06-03 13:49:03 +0200

    added recordsignal and heartrate

--- patches/biochill/startup.sh
+++ patches/biochill/startup.sh
@@ -6,3 +6,5 @@ $PATCHDIR/terminal.scpt $PATCHDIR/redis.sh
 $PATCHDIR/terminal.scpt $PATCHDIR/buffer.sh
 $PATCHDIR/terminal.scpt $PATCHDIR/bitalino2ft.sh
 $PATCHDIR/terminal.scpt $PATCHDIR/plotsignal.sh
+$PATCHDIR/terminal.scpt $PATCHDIR/recordsignal.sh
+$PATCHDIR/terminal.scpt $PATCHDIR/heartrate.sh
