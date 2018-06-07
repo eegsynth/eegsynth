93d95fe336bafd4b1136d6a1b15029b9f4793cf2
Robert Oostenveld <r.oostenveld@gmail.com>
2018-06-07 10:13:51 +0300

    beautify

--- module/recordsignal/recordsignal.py
+++ module/recordsignal/recordsignal.py
@@ -19,7 +19,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import ConfigParser # this is version 2.x specific, on version 3.x it is called "configparser" and has a different API
+import ConfigParser  # this is version 2.x specific, on version 3.x it is called "configparser" and has a different API
 import argparse
 import datetime
 import numpy as np
@@ -32,32 +32,33 @@ import struct
 
 if hasattr(sys, 'frozen'):
     basis = sys.executable
-elif sys.argv[0]!='':
+elif sys.argv[0] != '':
     basis = sys.argv[0]
 else:
     basis = './'
 installed_folder = os.path.split(basis)[0]
 
 # eegsynth/lib contains shared modules
-sys.path.insert(0, os.path.join(installed_folder,'../../lib'))
+sys.path.insert(0, os.path.join(installed_folder, '../../lib'))
 import EEGsynth
 import FieldTrip
 import EDF
 
-MININT16 = -np.power(2,15)
-MAXINT16 =  np.power(2,15)-1
-MININT32 = -np.power(2,31)
-MAXINT32 =  np.power(2,31)-1
+MININT16 = -np.power(2, 15)
+MAXINT16 = np.power(2, 15) - 1
+MININT32 = -np.power(2, 31)
+MAXINT32 = np.power(2, 31) - 1
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-i", "--inifile", default=os.path.join(installed_folder, os.path.splitext(os.path.basename(__file__))[0] + '.ini'), help="optional name of the configuration file")
+parser.add_argument("-i", "--inifile", default=os.path.join(installed_folder,
+                                                            os.path.splitext(os.path.basename(__file__))[0] + '.ini'), help="optional name of the configuration file")
 args = parser.parse_args()
 
 config = ConfigParser.ConfigParser()
 config.read(args.inifile)
 
 try:
-    r = redis.StrictRedis(host=config.get('redis','hostname'), port=config.getint('redis','port'), db=0)
+    r = redis.StrictRedis(host=config.get('redis', 'hostname'), port=config.getint('redis', 'port'), db=0)
     response = r.client_list()
 except redis.ConnectionError:
     print "Error: cannot connect to redis server"
@@ -68,19 +69,19 @@ patch = EEGsynth.patch(config, r)
 del config
 
 # this determines how much debugging information gets printed
-debug = patch.getint('general','debug')
+debug = patch.getint('general', 'debug')
 
 # this is the timeout for the FieldTrip buffer
-timeout = patch.getfloat('fieldtrip','timeout')
+timeout = patch.getfloat('fieldtrip', 'timeout')
 
 try:
-    ftc_host = patch.getstring('fieldtrip','hostname')
-    ftc_port = patch.getint('fieldtrip','port')
-    if debug>0:
+    ftc_host = patch.getstring('fieldtrip', 'hostname')
+    ftc_port = patch.getint('fieldtrip', 'port')
+    if debug > 0:
         print 'Trying to connect to buffer on %s:%i ...' % (ftc_host, ftc_port)
     ftc = FieldTrip.Client()
     ftc.connect(ftc_host, ftc_port)
-    if debug>0:
+    if debug > 0:
         print "Connected to FieldTrip buffer"
 except:
     print "Error: cannot connect to FieldTrip buffer"
@@ -89,17 +90,17 @@ except:
 hdr_input = None
 start = time.time()
 while hdr_input is None:
-    if debug>0:
+    if debug > 0:
         print "Waiting for data to arrive..."
-    if (time.time()-start)>timeout:
+    if (time.time() - start) > timeout:
         print "Error: timeout while waiting for data"
         raise SystemExit
     hdr_input = ftc.getHeader()
     time.sleep(0.2)
 
-if debug>0:
+if debug > 0:
     print "Data arrived"
-if debug>1:
+if debug > 1:
     print hdr_input
     print hdr_input.labels
 
@@ -119,21 +120,21 @@ while True:
     hdr_input = ftc.getHeader()
 
     if recording and hdr_input is None:
-        if debug>0:
+        if debug > 0:
             print "Header is empty - closing", fname
         f.close()
         recording = False
         continue
 
     if recording and not patch.getint('recording', 'record'):
-        if debug>0:
+        if debug > 0:
             print "Recording disabled - closing", fname
         f.close()
         recording = False
         continue
 
     if not recording and not patch.getint('recording', 'record'):
-        if debug>0:
+        if debug > 0:
             print "Recording is not enabled"
         time.sleep(1)
 
@@ -142,78 +143,78 @@ while True:
         # open a new file
         fname = patch.getstring('recording', 'file')
         name, ext = os.path.splitext(fname)
-        if len(ext)==0:
+        if len(ext) == 0:
             ext = '.' + fileformat
         fname = name + '_' + datetime.datetime.now().strftime("%Y.%m.%d_%H.%M.%S") + ext
         # the blocksize depends on the sampling rate, which may have changed
-        blocksize = int(patch.getfloat('recording', 'blocksize')*hdr_input.fSample)
+        blocksize = int(patch.getfloat('recording', 'blocksize') * hdr_input.fSample)
         # write the header to file
-        if debug>0:
+        if debug > 0:
             print "Opening", fname
-        if fileformat=='edf':
+        if fileformat == 'edf':
             # construct the header
             meas_info = {}
             chan_info = {}
-            meas_info['record_length']  = blocksize/hdr_input.fSample
-            meas_info['nchan']          = hdr_input.nChannels
+            meas_info['record_length'] = blocksize / hdr_input.fSample
+            meas_info['nchan'] = hdr_input.nChannels
             now = datetime.datetime.now()
-            meas_info['year']           = now.year
-            meas_info['month']          = now.month
-            meas_info['day']            = now.day
-            meas_info['hour']           = now.hour
-            meas_info['minute']         = now.minute
-            meas_info['second']         = now.second
-            chan_info['physical_min']   = hdr_input.nChannels * [physical_min]
-            chan_info['physical_max']   = hdr_input.nChannels * [physical_max]
-            chan_info['digital_min']    = hdr_input.nChannels * [MININT16]
-            chan_info['digital_max']    = hdr_input.nChannels * [MAXINT16]
-            chan_info['ch_names']       = hdr_input.labels
-            chan_info['n_samps']        = hdr_input.nChannels * [blocksize]
+            meas_info['year'] = now.year
+            meas_info['month'] = now.month
+            meas_info['day'] = now.day
+            meas_info['hour'] = now.hour
+            meas_info['minute'] = now.minute
+            meas_info['second'] = now.second
+            chan_info['physical_min'] = hdr_input.nChannels * [physical_min]
+            chan_info['physical_max'] = hdr_input.nChannels * [physical_max]
+            chan_info['digital_min'] = hdr_input.nChannels * [MININT16]
+            chan_info['digital_max'] = hdr_input.nChannels * [MAXINT16]
+            chan_info['ch_names'] = hdr_input.labels
+            chan_info['n_samps'] = hdr_input.nChannels * [blocksize]
             print chan_info
             f = EDF.EDFWriter(fname)
             f.writeHeader((meas_info, chan_info))
-        elif fileformat=='wav':
+        elif fileformat == 'wav':
             f = wave.open(fname, 'w')
             f.setnchannels(hdr_input.nChannels)
             f.setnframes(0)
-            f.setsampwidth(4) # 1, 2 or 4
+            f.setsampwidth(4)  # 1, 2 or 4
             f.setframerate(hdr_input.fSample)
         else:
             raise NotImplementedError('unsupported file format')
         # determine the starting point for recording
-        if hdr_input.nSamples>blocksize:
+        if hdr_input.nSamples > blocksize:
             endsample = hdr_input.nSamples - 1
             begsample = endsample - blocksize + 1
         else:
             endsample = blocksize - 1
             begsample = endsample - blocksize + 1
 
-    if recording and hdr_input.nSamples<begsample-1:
-        if debug>0:
+    if recording and hdr_input.nSamples < begsample - 1:
+        if debug > 0:
             print "Header was reset - closing", fname
         f.close()
         recording = False
         continue
 
-    if recording and endsample>hdr_input.nSamples-1:
-        if debug>2:
+    if recording and endsample > hdr_input.nSamples - 1:
+        if debug > 2:
             print "Waiting for data", endsample, hdr_input.nSamples
-        time.sleep((endsample-hdr_input.nSamples)/hdr_input.fSample)
+        time.sleep((endsample - hdr_input.nSamples) / hdr_input.fSample)
         continue
 
     if recording:
         D = ftc.getData([begsample, endsample])
-        if debug>0:
+        if debug > 0:
             print "Writing sample", begsample, "to", endsample, "as", np.shape(D)
-        if fileformat=='edf':
+        if fileformat == 'edf':
             f.writeBlock(np.transpose(D))
-        elif fileformat=='wav':
+        elif fileformat == 'wav':
             for sample in range(len(D)):
-                x = D[sample,:]
+                x = D[sample, :]
                 # scale the floating point values between -1 and 1
-                y = x / ((physical_max-physical_min)/2)
+                y = x / ((physical_max - physical_min) / 2)
                 # scale the floating point values between MININT32 and MAXINT32
-                y = y * ((float(MAXINT32)-float(MININT32))/2)
+                y = y * ((float(MAXINT32) - float(MININT32)) / 2)
                 # convert them to packed binary data
                 z = "".join((wave.struct.pack('i', item) for item in y))
                 f.writeframes(z)
