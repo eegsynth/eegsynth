7aac197c8db0d7ff042a39a8529a8da80c424038
Robert Oostenveld <r.oostenveld@gmail.com>
2018-06-11 13:57:44 +0200

    cleaned up patches for biochill project

--- patches/biochill/redis.sh
+++ patches/biochill/redis.sh
@@ -5,4 +5,16 @@ MODULE=`basename $0 .sh`
 EEGSYNTH=$HOME/eegsynth
 INIDIR=`dirname $0`
 
-$EEGSYNTH/module/$MODULE/$MODULE.sh -i $INIDIR/$MODULE.ini
+# $EEGSYNTH/module/$MODULE/$MODULE.sh -i $INIDIR/$MODULE.ini
+
+# the redis.conf contains details on the bind address, writing the database to disk, etc.
+
+if [ `which redis-server` == /opt/local/bin/redis-server ]
+then 
+  CONFIG=/opt/local/etc/redis.conf
+else
+  CONFIG=""
+fi
+
+echo starting \"redis-server $CONFIG\"
+redis-server $CONFIG --loglevel verbose
