ab44831fa3e1f74525356f7268db787c39604ac4
Robert Oostenveld <r.oostenveld@gmail.com>
2018-06-24 17:39:07 +0200

    do not update the scale/offset/prefix upon every incoming message, but only once per second or so

--- module/inputosc/inputosc.py
+++ module/inputosc/inputosc.py
@@ -19,7 +19,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import ConfigParser # this is version 2.x specific, on version 3.x it is called "configparser" and has a different API
+import ConfigParser  # this is version 2.x specific, on version 3.x it is called "configparser" and has a different API
 import OSC          # see https://trac.v2.nl/wiki/pyOSC
 import argparse
 import os
@@ -31,25 +31,26 @@ import time
 
 if hasattr(sys, 'frozen'):
     basis = sys.executable
-elif sys.argv[0]!='':
+elif sys.argv[0] != '':
     basis = sys.argv[0]
 else:
     basis = './'
 installed_folder = os.path.split(basis)[0]
 
 # eegsynth/lib contains shared modules
-sys.path.insert(0, os.path.join(installed_folder,'../../lib'))
+sys.path.insert(0, os.path.join(installed_folder, '../../lib'))
 import EEGsynth
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-i", "--inifile", default=os.path.join(installed_folder, os.path.splitext(os.path.basename(__file__))[0] + '.ini'), help="optional name of the configuration file")
+parser.add_argument("-i", "--inifile", default=os.path.join(installed_folder,
+                                                            os.path.splitext(os.path.basename(__file__))[0] + '.ini'), help="optional name of the configuration file")
 args = parser.parse_args()
 
 config = ConfigParser.ConfigParser()
 config.read(args.inifile)
 
 try:
-    r = redis.StrictRedis(host=config.get('redis','hostname'), port=config.getint('redis','port'), db=0)
+    r = redis.StrictRedis(host=config.get('redis', 'hostname'), port=config.getint('redis', 'port'), db=0)
     response = r.client_list()
 except redis.ConnectionError:
     print "Error: cannot connect to redis server"
@@ -60,11 +61,11 @@ patch = EEGsynth.patch(config, r)
 del config
 
 # this determines how much debugging information gets printed
-debug = patch.getint('general','debug')
+debug = patch.getint('general', 'debug')
 
 try:
-    s = OSC.OSCServer((patch.getstring('osc','address'), patch.getint('osc','port')))
-    if debug>0:
+    s = OSC.OSCServer((patch.getstring('osc', 'address'), patch.getint('osc', 'port')))
+    if debug > 0:
         print "Started OSC server"
 except:
     print "Unexpected error:", sys.exc_info()[0]
@@ -74,32 +75,43 @@ except:
 # this is a list of OSC messages that are to be processed as button presses, i.e. using a pubsub message in redis
 button_list = patch.getstring('button', 'push').split(',')
 
+# the scale and offset are used to map OSC values to Redis values
+scale = patch.getfloat('output', 'scale', default=1)
+offset = patch.getfloat('output', 'offset', default=0)
+
+# the results will be written to redis as "osc.1.faderA" etc.
+prefix = patch.getstring('output', 'prefix')
+
+# the scale, offset and prefix are updated every N seconds
+update = patch.getfloat('general', 'update', default=1)
+
 # define a message-handler function for the server to call.
+
+
 def forward_handler(addr, tags, data, source):
-    if debug>1:
-      print "---"
-      print "source %s" % OSC.getUrlStr(source)
-      print "addr   %s" % addr
-      print "tags   %s" % tags
-      print "data   %s" % data
+    global prefix
+    global scake
+    global offset
 
-    if addr[0]!='/':
-      # ensure it starts with a slash
-      addr = '/' + addr
+    if debug > 1:
+        print "---"
+        print "source %s" % OSC.getUrlStr(source)
+        print "addr   %s" % addr
+        print "tags   %s" % tags
+        print "data   %s" % data
 
-    # the scale and offset are used to map OSC values to Redis values
-    scale  = patch.getfloat('output', 'scale', default=1)
-    offset = patch.getfloat('output', 'offset', default=0)
+    if addr[0] != '/':
+        # ensure it starts with a slash
+        addr = '/' + addr
 
-    # the results will be written to redis as "osc.1.faderA" etc.
-    key = patch.getstring('output', 'prefix') + addr.replace('/', '.')
+    key = prefix + addr.replace('/', '.')
 
-    if tags=='f' or tags=='i':
-        # it is a single value
+    if tags == 'f' or tags == 'i':
+            # it is a single value
         val = EEGsynth.rescale(data[0], slope=scale, offset=offset)
         r.set(key, val)             # send it as control value
         if addr in button_list:
-            r.publish(key,val)      # send it as trigger
+            r.publish(key, val)      # send it as trigger
 
     else:
         for i in range(len(data)):
@@ -108,6 +120,7 @@ def forward_handler(addr, tags, data, source):
             # append the index to the key, this starts with 0
             r.set(key + '.%i' % i, val)
 
+
 s.noCallback_handler = forward_handler
 s.addDefaultHandlers()
 # s.addMsgHandler("/1/faderA", test_handler)
@@ -115,21 +128,26 @@ s.addDefaultHandlers()
 # just checking which handlers we have added
 print "Registered Callback-functions are :"
 for addr in s.getOSCAddressSpace():
-	print addr
+    print addr
 
 # start the server thread
 print "\nStarting module. Use ctrl-C to quit."
-st = threading.Thread( target = s.serve_forever )
+st = threading.Thread(target=s.serve_forever)
 st.start()
 
-# loop while threads are running
+# loop while incoming OSC messages are being handled
 try:
-	while True:
-		time.sleep(10)
+    while True:
+        time.sleep(update)
+        # the scale and offset are used to map OSC values to Redis values
+        scale = patch.getfloat('output', 'scale', default=1)
+        offset = patch.getfloat('output', 'offset', default=0)
+        # the results will be written to redis as "osc.1.faderA" etc.
+        prefix = patch.getstring('output', 'prefix')
 
 except KeyboardInterrupt:
-	print "\nClosing module."
-	s.close()
-	print "Waiting for Server-thread to finish."
-	st.join()
-	print "Done."
+    print "\nClosing module."
+    s.close()
+    print "Waiting for Server-thread to finish."
+    st.join()
+    print "Done."
