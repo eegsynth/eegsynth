dff1aacec8a2a7c8ac33d2bac186ea43651bf639
JohnDoenut <j.brammer@psych.ru.nl>
2019-08-23 17:06:26 +0200

    re-implemented real-time breathing rate estimation

--- module/biofeedback/resp_online_dev.py
+++ module/biofeedback/resp_online_dev.py
@@ -8,61 +8,64 @@ This is a temporary script file.
 import numpy as np
 import matplotlib.pyplot as plt
 from filters import butter_lowpass_filter
+from scipy.signal import argrelextrema, peak_prominences
 
 
 def extrema_signal(signal, sfreq, enable_plot=False):
     '''
-    the goal is to provide real-time estimates of a) breathing rate, and b)
-    the current phase of breathing (inspiration, expiration); a is
-    estimated based on negative zero-crossings of the signal's gradient (i.e.,
-    one breathing cycle is defined as peak-to-peak difference, since peaks are
-    more sharply defined than troughs with the Bitalino belt []); b) is simply
-    the gradient of the signal at any time
+    the goal is to provide a real-time estimates of breathing rate, based on
+    the inhalation peaks; peaks are chosen over troughs since the former are
+    more sharply defined than troughs in signals acquired with the Bitalino
+    belt (link)
     
+    detection is based on online averages of...
+    
+    ... breathing rate: throw out extrema that follow another extreme by
+        half the period of the average rate
+    ... peak prominence (link) : throw out extrema that have a prominen smaller
+        than weight * average prominence
+        
+    update online average according to D. Knuth; https://dsp.stackexchange.com/
+    questions/811/determining-the-mean-and-standard-deviation-in-real-time
+    
+    the algorithm can be run as a real-time simulation like so
+    (enable_plot=True for visual introspection):
     path = 
     channel = 
-    data = np.loadtxt(path)[:, channel]
-    extrema_signal(data, 1000, enable_plot=True)
-    
-    online average of...
-    
-    ... breathing frequency: throw out extrema that follow another extreme by
-        half the period of that average frequency
-    ... amplitude difference: throw out extrema that have a difference smaller
-        than weight * average amplitude difference from the preceding extrema
+    sfreq
+    data = np.loadtxt(path)[channel]
+    extrema_signal(data, sfreq, enable_plot=True)
 
     '''
-
   
     # enforce numpy and vector-form
     signal = np.ravel(signal)
     sfreq = 1000
 
     # set free parameters
-    window_size = np.ceil(3 * sfreq)
+    window_size = int(np.ceil(3 * sfreq))
     # amount of samples to shift the window on each iteration
-    stride = np.ceil(1 * sfreq)
-    extend = int(np.ceil(0.2 * sfreq))
+    stride = np.ceil(0.5 * sfreq)
+    promweight = 0.4
 
-    # initiate stuff
-    block = 0
-    last_extreme = 0
-    gradthresh = 0    
-    peaks = []
+    # initiate variables
+    block = 0  
+    avgrate = 15
+    avgprom = 0
+    lastpeak = 0
+    validblocks = 0
     
-    # simulate online filtering (later done by preprocessing module);
-    # filter out fluctuations that occur faster than 0.5 Hz (breathing
-    # rate faster than 30)
-    signal = butter_lowpass_filter(signal, 0.5, sfreq)
-    
-    
+    # initiate plot
     if enable_plot is True:
         ax1 = plt.subplot(211)
         ax2 = plt.subplot(212, sharex=ax1)
-        # plot line to visualize zero crossing
-        ax2.axhline(y = 0)
-        
-
+    
+    # simulate online filtering (later done by preprocessing module); filter
+    # out fluctuations that occur faster than 0.5 Hz (breathing rate faster
+    # than 30)
+    signal = butter_lowpass_filter(signal, 0.5, sfreq)
+    
+    # start real-time simulation
     while block * stride + window_size <= len(signal):
         block_idcs = np.arange(block * stride,
                                block * stride + window_size,
@@ -71,46 +74,67 @@ def extrema_signal(signal, sfreq, enable_plot=False):
         block_sec = block_idcs / sfreq
         
         x = signal[block_idcs]
-        grad = np.gradient(x)
         
+        # plot signal
         if enable_plot is True:
             ax1.plot(block_sec, x, c='r')
-            ax2.plot(block_sec, grad)
         
-        # get negative zero crossings
-        extrema = neg_crossings(grad)
-                    
-        if extrema.size > 0:
-            extreme = extrema[-1]
-            horzdiff = block_idcs[extreme] - last_extreme
-            if enable_plot is True:
-                ax1.scatter(block_sec[extreme], x[extreme], c='y', marker='P',
-                            s=200)
-            # get average gradient in 200msec preceding each negative zero
-            # crossing
-            if grad[extreme - extend:extreme].size > 0:
-                avggrad = np.average(grad[extreme - extend:extreme])
-                # if the current temporal difference exceeds a second, consider
-                # the current zero crossing a valid extreme; also the average 
-                # gradient preceding the extreme must exceeed a threshold
-                if (horzdiff > 1 * sfreq) and (avggrad > 0.3 * gradthresh):
-                    # update running mean of gradient threshold (Knuth; 
-                    # https://dsp.stackexchange.com/questions/811/determining-
-                    # the-mean-and-standard-deviation-in-real-time)
-                    gradthresh = (gradthresh + (avggrad - gradthresh) /
-                                  (block + 1))
-                    peaks.append(block_idcs[extreme])
-                    if enable_plot is True:
-                        ax1.scatter(block_sec[extreme], x[extreme], c='m',
-                                    marker='X', s=200)
-
-            if peaks:
-                last_extreme = peaks[-1]
-
+        # identify peaks
+        peaks = argrelextrema(x, np.greater)[0]
+        
+        # if no peak was detected jump to the next block 
+        if peaks.size < 1:
+            block += 1
+            continue
+        
+        # index of peak relative to block and absolute to recording
+        peakidx = peaks[-1]
+        peak = block_idcs[peakidx]
+            
+        # if no new peak was detected jump to next block
+        if peak - lastpeak == 0:
+            block += 1
+            continue
+        
+        # plot new peak
+        if enable_plot is True:
+            ax1.scatter(block_sec[peakidx], x[peakidx], c='y', marker='P',
+                        s=200)
+        
+        # if current peak occurs too close to last peak jump to next block
+        rate = (60 / ((peak - lastpeak) / sfreq))
+        if rate > 2 * avgrate:
+            block += 1
+            continue
+        
+        # plot peaks that made it past the rate threshold
+        if enable_plot is True:
+            ax1.scatter(block_sec[peakidx], x[peakidx], c='g', marker='X', 
+                        s=200)
+        
+        # determine the prominence of each peak
+        prom = peak_prominences(x, [peakidx])[0]
+        # if current peak prominence is too small, jump to next block; larger
+        # promweight is more conservative (throws out more peaks)      
+        if prom < avgprom * promweight:
+            block += 1
+            continue
+        
+        # update average parameters
+        avgprom = (avgprom + (prom - avgprom) / (validblocks + 1))
+        avgrate = (avgrate + (rate - avgrate) / (validblocks + 1))
+        lastpeak = peak
+        # for the real-time averages it is important to keep a counter of only
+        # the valid blocks (blocks on which peak made it past both thresholds)
+        validblocks += 1
+        
+        # plot average parameters and peaks that made it past the prominence
+        # threshold
+        if enable_plot is True:
+            ax1.scatter(block_sec[peakidx], x[peakidx], c='m', marker='X', 
+                        s=200)
+            ax2.plot(block_sec, np.ones(window_size) * avgrate, c='b')
+            ax2.plot(block_sec, np.ones(window_size) * avgprom, c='g')
+            
         block += 1
         
-    return peaks
-
-def neg_crossings(signal):
-        pos = signal > 0
-        return (pos[:-1] & ~pos[1:]).nonzero()[0]
