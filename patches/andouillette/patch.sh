574912fc1335ef2190df61b15b9ffc3d5966f274
Robert Oostenveld <r.oostenveld@gmail.com>
2019-09-28 16:28:54 +0300

    updated with last-minute changes to andouilette

--- patches/andouillette/patch.sh
+++ patches/andouillette/patch.sh
@@ -7,7 +7,7 @@ $PATCHDIR/terminal.scpt $PATCHDIR/redis.sh
 $PATCHDIR/terminal.scpt $PATCHDIR/audio2ft.sh
 $PATCHDIR/terminal.scpt $PATCHDIR/inputcontrol.sh
 $PATCHDIR/terminal.scpt $PATCHDIR/lsl2ft.sh
-#$PATCHDIR/terminal.scpt $PATCHDIR/playbacksignal.sh
+# $PATCHDIR/terminal.scpt $PATCHDIR/playbacksignal.sh
 
 echo Waiting for a few seconds ...
 sleep 10
@@ -23,7 +23,7 @@ sleep 5
 $PATCHDIR/terminal.scpt $PATCHDIR/geomixer.sh
 $PATCHDIR/terminal.scpt $PATCHDIR/quantizer.sh
 $PATCHDIR/terminal.scpt $PATCHDIR/historycontrol.sh
-# $PATCHDIR/terminal.scpt $PATCHDIR/historysignal.sh
+$PATCHDIR/terminal.scpt $PATCHDIR/historysignal.sh
 $PATCHDIR/terminal.scpt $PATCHDIR/postprocessing.sh
 $PATCHDIR/terminal.scpt $PATCHDIR/slewlimiter.sh
 
