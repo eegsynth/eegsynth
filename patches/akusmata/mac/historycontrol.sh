d83ca3b04068f9d1140b4bd09bed82f2bbf0d757
Robert Oostenveld <r.oostenveld@gmail.com>
2019-09-30 10:51:24 +0200

    documented one of the two patches used at the Akusmata performande

---
+++ patches/akusmata/mac/historycontrol.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export RTMIDI_API=MACOSX_CORE
+export DYLD_LIBRARY_PATH=/System/Library/Frameworks/ImageIO.framework/Versions/A/Resources
+export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:/usr/local/lib
+
+MODULE=`basename $0 .sh`
+
+EEGSYNTH=$HOME/eegsynth
+INIDIR=`dirname $0`
+
+$EEGSYNTH/module/$MODULE/$MODULE.py -i $INIDIR/$MODULE.ini
