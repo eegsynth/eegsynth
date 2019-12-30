7d16f41e09a03aec0fad084709fbab4cbb50a223
Robert Oostenveld <r.oostenveld@gmail.com>
2019-12-30 23:29:52 +0100

    added __init__.py file for each of the EEGsynth modules, it contains a single class that can be used to start the module

---
+++ module/quantizer/__init__.py
@@ -0,0 +1,8 @@
+import sys
+
+class Module:
+    def __init__(self, args=None):
+        if args!=None:
+            # override the command line arguments
+            sys.argv = [sys.argv[0]] + args
+        from . import quantizer
