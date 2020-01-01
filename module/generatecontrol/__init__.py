dd3857ee5afc2b041aa7f76996755e1ad23f3629
Robert Oostenveld <r.oostenveld@gmail.com>
2020-01-01 14:53:46 +0100

    small cleanup to __init__.py

--- module/generatecontrol/__init__.py
+++ module/generatecontrol/__init__.py
@@ -1,8 +1,9 @@
 import sys
 
-class Module:
+class Executable:
     def __init__(self, args=None):
         if args!=None:
             # override the command line arguments
             sys.argv = [sys.argv[0]] + args
+        # the module is implemented as a Python script and starts as soon as it is imported
         from . import generatecontrol
