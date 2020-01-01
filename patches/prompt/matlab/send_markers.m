ad85ca763fb37ad8129dc53fb15fa3b102d03c0f
Robert Oostenveld <r.oostenveld@gmail.com>
2020-01-01 17:27:02 +0100

    updated matlab code for the PROMPT patch

--- patches/prompt/matlab/send_markers.m
+++ patches/prompt/matlab/send_markers.m
@@ -1,15 +1,21 @@
-% instantiate the library
-disp('Loading library...');
-lib = lsl_loadlib();
+addpath(genpath('~/matlab/liblsl'))
 
-disp('Creating the marker stream info...');
-info = lsl_streaminfo(lib, 'experiment', 'markers', 1, 0, 'cf_string', '27652');
+if ~exist('outlet')
+  % instantiate the library
+  disp('Loading library...');
+  lib = lsl_loadlib();
+  
+  disp('Creating the marker stream info...');
+  info = lsl_streaminfo(lib, 'experiment', 'markers', 1, 0, 'cf_string', '27652');
+  
+  disp('Opening an outlet...');
+  outlet = lsl_outlet(info);
+  
+  % this is needed, otherwise the first marker is not sent/received
+  pause(3)
+end
 
-disp('Opening an outlet...');
-outlet = lsl_outlet(info);
-
-% this is needed, otherwise the first marker is not sent/received
-pause(2)
+%%
 
 % send markers into the outlet
 disp('Now transmitting data...');
@@ -18,10 +24,10 @@ disp('start');
 outlet.push_sample({'start'});
 pause(1)
 
-for i=1:10
+for i=1:100
   disp('sync');
   outlet.push_sample({'sync'});
-  pause(1)
+  pause(0.5)
 end
 
 disp('stop');
