85fcd06a6334c45f5f277e1838540eff8c687d18
Robert Oostenveld <r.oostenveld@gmail.com>
2022-02-20 16:45:35 +0100

    implemented brainflow2ft module to support OpenBCI, FreeEEG32, Unicorn, Muse, and various other EEG hardware devices

---
+++ module/brainflow2ft/README.md
@@ -0,0 +1,7 @@
+# brainflow2ft module
+
+The purpose of this module is to read real-time data from any EEG system supported by [BrainFlow](https://brainflow.readthedocs.io/) and to write it to the FieldTrip buffer. Other modules can read it from the buffer and analyze/convert/process it.
+
+# Requirements
+
+The FieldTrip buffer should be running prior to starting this module.
---
+++ module/brainflow2ft/brainflow2ft.py
@@ -0,0 +1,203 @@
+#!/usr/bin/env python
+
+# Brainflow2ft reads data from an EEG device and writes it to a FieldTrip buffer
+#
+# This software is part of the EEGsynth project, see <https://github.com/eegsynth/eegsynth>.
+#
+# Copyright (C) 2022 EEGsynth project
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import configparser
+import argparse
+import numpy as np
+import redis
+import os
+import sys
+import time
+
+import brainflow
+from brainflow.board_shim import BoardShim, BrainFlowInputParams
+from brainflow.data_filter import DataFilter, FilterTypes, AggOperations
+
+if hasattr(sys, "frozen"):
+    path = os.path.split(sys.executable)[0]
+    file = os.path.split(sys.executable)[-1]
+    name = os.path.splitext(file)[0]
+elif __name__ == "__main__" and sys.argv[0] != "":
+    path = os.path.split(sys.argv[0])[0]
+    file = os.path.split(sys.argv[0])[-1]
+    name = os.path.splitext(file)[0]
+elif __name__ == "__main__":
+    path = os.path.abspath("")
+    file = os.path.split(path)[-1] + ".py"
+    name = os.path.splitext(file)[0]
+else:
+    path = os.path.split(__file__)[0]
+    file = os.path.split(__file__)[-1]
+    name = os.path.splitext(file)[0]
+
+# eegsynth/lib contains shared modules
+sys.path.insert(0, os.path.join(path, "../../lib"))
+import EEGsynth
+import FieldTrip
+
+def _setup():
+    """Initialize the module
+    This adds a set of global variables
+    """
+    global parser, args, config, r, response, patch
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-i",
+        "--inifile",
+        default=os.path.join(path, name + ".ini"),
+        help="name of the configuration file",
+    )
+    args = parser.parse_args()
+
+    config = configparser.ConfigParser(inline_comment_prefixes=("#", ";"))
+    config.read(args.inifile)
+
+    try:
+        r = redis.StrictRedis(
+            host=config.get("redis", "hostname"),
+            port=config.getint("redis", "port"),
+            db=0,
+            charset="utf-8",
+            decode_responses=True,
+        )
+        response = r.client_list()
+    except redis.ConnectionError:
+        raise RuntimeError("cannot connect to Redis server")
+
+    # combine the patching from the configuration file and Redis
+    patch = EEGsynth.patch(config, r)
+
+    # there should not be any local variables in this function, they should all be global
+    if len(locals()):
+        print("LOCALS: " + ", ".join(locals().keys()))
+
+
+def _start():
+    """Start the module
+    This uses the global variables from setup and adds a set of global variables
+    """
+    global parser, args, config, r, response, patch
+    global monitor, debug, delay, ft_host, ft_port, ft_output, board_id, streamer_params, params, board
+
+    # this can be used to show parameters that have changed
+    monitor = EEGsynth.monitor(name=name, debug=patch.getint("general", "debug"))
+
+    # get the general options from the configuration file
+    debug = patch.getint("general", "debug")
+    delay = patch.getfloat("general", "delay")
+
+    # get the options that are specific for BrainFlow
+    board_id             = patch.getint("brainflow", "board_id", default=-1)
+    streamer_params      = patch.getstring("brainflow", "streamer_params", default=None)
+    params               = BrainFlowInputParams()
+    params.ip_port       = patch.getint("brainflow", "ip_port", default=0)
+    params.serial_port   = patch.getstring("brainflow", "board_id", default="/dev/cu.usbmodem11")
+    params.mac_address   = patch.getstring("brainflow", "mac_address", default="")
+    params.other_info    = patch.getstring("brainflow", "other_info", default="")
+    params.serial_number = patch.getstring("brainflow", "serial_number", default="")
+    params.ip_address    = patch.getstring("brainflow", "ip_address", default="")
+    params.ip_protocol   = patch.getint("brainflow", "ip_protocol", default=0)
+    params.timeout       = patch.getint("brainflow", "timeout", default=0)
+    params.file          = patch.getstring("brainflow", "file", default="")
+
+    monitor.success("Starting BrainFlow ...")
+
+    BoardShim.disable_board_logger()
+    # BoardShim.enable_board_logger()
+    # BoardShim.enable_dev_board_logger()
+
+    params = BrainFlowInputParams()
+    params.serial_port = '/dev/cu.usbmodem11'
+
+    board = BoardShim(1, params)
+    board.prepare_session()
+    board.start_stream(45000, streamer_params)
+    
+    # get all data and remove it from internal buffer
+    board.get_board_data()
+
+    monitor.success("Connected to " + board.get_device_name(board_id))
+    monitor.info("fsample =", board.get_sampling_rate(board_id))
+    monitor.info("nchans  =", board.get_num_rows(board_id))
+
+    try:
+        ft_host = patch.getstring("fieldtrip", "hostname")
+        ft_port = patch.getint("fieldtrip", "port")
+        monitor.success("Trying to connect to buffer on %s:%i ..." % (ft_host, ft_port))
+        ft_output = FieldTrip.Client()
+        ft_output.connect(ft_host, ft_port)
+        monitor.success("Connected to output FieldTrip buffer")
+    except:
+        raise RuntimeError("cannot connect to output FieldTrip buffer")
+    
+    ft_output.putHeader(board.get_num_rows(board_id), float(board.get_sampling_rate(board_id)), FieldTrip.DATATYPE_FLOAT32)
+
+    # there should not be any local variables in this function, they should all be global
+    if len(locals()):
+        print("LOCALS: " + ", ".join(locals().keys()))
+
+
+def _loop_once():
+    """Run the main loop once
+    This uses the global variables from setup and start, and adds a set of global variables
+    """
+    global parser, args, config, r, response, patch
+    global monitor, delay, ft_output, board, data
+    
+    if board.get_board_data_count()>0:
+        data = np.transpose(board.get_board_data())
+        ft_output.putData(data.astype(np.float32))
+        monitor.debug("wrote " + str(len(data[:,1])) + " samples")
+    else:
+        # wait for a short time before trying again
+        time.sleep(delay)
+
+    # there should not be any local variables in this function, they should all be global
+    if len(locals()):
+        print("LOCALS: " + ", ".join(locals().keys()))
+
+
+def _loop_forever():
+    """Run the main loop forever
+    """
+    global monitor
+    while True:
+        monitor.loop()
+        _loop_once()
+
+
+def _stop():
+    """Stop and clean up on SystemExit, KeyboardInterrupt
+    """
+    global board
+    board.stop_stream()
+    board.release_session()
+    sys.exit()
+
+
+if __name__ == "__main__":
+    _setup()
+    _start()
+    try:
+        _loop_forever()
+    except (SystemExit, KeyboardInterrupt, RuntimeError):
+        _stop()
