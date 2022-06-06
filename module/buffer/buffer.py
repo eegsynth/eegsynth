239a16c0d16fe93f7dc4280abd47d8497380399b
Robert Oostenveld <r.oostenveld@gmail.com>
2022-06-06 14:09:26 +0200

    implemented FieldTrip buffer module for EEGsynth in pure Python

---
+++ module/buffer/buffer.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python
+
+# Buffer starts a FieldTrip buffer server
+#
+# This software is part of the EEGsynth project, see <https://github.com/eegsynth/eegsynth>.
+#
+# Copyright (C) 2022 EEGsynth project
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import configparser
+import argparse
+import os
+import redis
+import sys
+import time
+import threading
+
+if hasattr(sys, 'frozen'):
+    path = os.path.split(sys.executable)[0]
+    file = os.path.split(sys.executable)[-1]
+    name = os.path.splitext(file)[0]
+elif __name__ == '__main__' and sys.argv[0] != '':
+    path = os.path.split(sys.argv[0])[0]
+    file = os.path.split(sys.argv[0])[-1]
+    name = os.path.splitext(file)[0]
+elif __name__ == '__main__':
+    path = os.path.abspath('')
+    file = os.path.split(path)[-1] + '.py'
+    name = os.path.splitext(file)[0]
+else:
+    path = os.path.split(__file__)[0]
+    file = os.path.split(__file__)[-1]
+    name = os.path.splitext(file)[0]
+
+# eegsynth/lib contains shared modules
+sys.path.insert(0, os.path.join(path, '../../lib'))
+import EEGsynth
+import FieldTrip
+
+
+def _setup():
+    '''Initialize the module
+    This adds a set of global variables
+    '''
+    global parser, args, config, r, response, patch
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--inifile", default=os.path.join(path, name + '.ini'), help="name of the configuration file")
+    args = parser.parse_args()
+
+    config = configparser.ConfigParser(inline_comment_prefixes=('#', ';'))
+    config.read(args.inifile)
+
+    try:
+        r = redis.StrictRedis(host=config.get('redis', 'hostname'), port=config.getint('redis', 'port'), db=0, charset='utf-8', decode_responses=True)
+        response = r.client_list()
+    except redis.ConnectionError:
+        raise RuntimeError("cannot connect to Redis server")
+
+    # combine the patching from the configuration file and Redis
+    patch = EEGsynth.patch(config, r)
+
+
+def _start():
+    '''Start the module
+    This uses the global variables from setup and adds a set of global variables
+    '''
+    global parser, args, config, r, response, patch
+    global monitor, debug, delay, port, server
+
+    # this can be used to show parameters that have changed
+    monitor = EEGsynth.monitor(name=name, debug=patch.getint('general', 'debug'))
+
+    # get the options from the configuration file
+    debug = patch.getint('general', 'debug')
+    delay = patch.getfloat('general', 'delay', default=0.010)
+    port = patch.getint('fieldtrip', 'port', multiple=True)
+    
+    server = []
+    for p in port:
+        monitor.info("starting server on %d" % p)
+        s = FieldTrip.Server()
+        s.connect(hostname='localhost', port=p)
+        s.timeout = 0 # the server main loop should not wait, as it would block the other instances
+        server.append(s);
+    del p, s
+        
+
+def _loop_once():
+    '''Run the main loop once
+    '''
+    global server
+    for s in server:
+        s.loop() # deal with new connections and incoming requests
+
+
+def _loop_forever():
+    '''Run the main loop forever
+    '''
+    global delay
+    while True:
+        monitor.loop()
+        _loop_once()
+        time.sleep(delay)
+
+
+def _stop():
+    '''Stop and clean up on SystemExit, KeyboardInterrupt
+    '''
+    global monitor, server
+    for s in server:
+        s.disconnect()
+    sys.exit()
+
+
+if __name__ == '__main__':
+    _setup()
+    _start()
+    try:
+        _loop_forever()
+    except (SystemExit, KeyboardInterrupt, RuntimeError):
+        _stop()
