4254b2aceed6504cfddb29e4a4dbc0e9b9280afe
Robert Oostenveld <r.oostenveld@gmail.com>
2024-09-15 21:42:11 +0200

    also write some data from the FT client test script

--- src/lib/FieldTrip_test_client.py
+++ src/lib/FieldTrip_test_client.py
@@ -11,13 +11,24 @@ while not client.isConnected and port<2000:
     except:
         port += 1
 
+print('-'*78)
+
+# write some data
+client.putHeader(2, 1000, FieldTrip.DATATYPE_FLOAT32)
+for block in range(10):
+    client.putData(np.random.rand(1000, 2).astype(np.float32))
+    print('wrote 1000 samples, 2 channels')
+print('-'*78)
+
 H = client.getHeader()
 print(H)
+print('-'*78)
 
 # read one second of data
 begsample = 0
 endsample = int(H.fSample-1)
 
 D = client.getData(index=(begsample, endsample))
+print(D.shape)
 print(D)
-print(len(D))
+print('-'*78)
