06049ea77926fbd5e3d603bb615b35dcec3a15f0
Robert Oostenveld <r.oostenveld@gmail.com>
2024-11-07 14:27:14 +0100

    fixed polarbelt module
    - address and uuid were messed up
    - some code cleanups and updates

--- src/module/polarbelt/README.md
+++ src/module/polarbelt/README.md
@@ -1,19 +1,14 @@
 # polarbelt module
 
-The purpose of this module is to process the heart rate and the inter-beat-intervals from a Polar H9 or H10 heart-rate band and write them to Redis channels.
+The purpose of this module is to process the heart rate and the inter-beat intervals from a Polar H9 or H10 heart-rate band and write them to Redis channels. Here are some helpful resources on how to access the data from the Polar belt via bluetooth:
 
-Helpful resources on how to access data via bluetooth:
-+ https://www.oreilly.com/library/view/getting-started-with/9781491900550/ch04.html
-+ https://stackoverflow.com/questions/52882552/getting-heart-rate-variability-from-polar-h10-uwp
-+ https://github.com/watsonix/node-ble-hr/blob/master/index.js
-+ https://github.com/Lawreros/PolarStrapBLE/blob/master/Scenario2_Client.xaml.cs
-+ https://www.raywenderlich.com/231-core-bluetooth-tutorial-for-ios-heart-rate-monitor
-+ https://github.com/danielfppps/hbpimon/blob/master/hb.py
-+ https://stackoverflow.com/questions/58778739/polar-h10-to-rpi-zero-w-hr-notifications-stop-after-ca-100s-without-error-mess
-+ https://stackoverflow.com/questions/56330018/how-to-read-and-convert-bluetooth-characteristic-from-byte-data-to-proper-values
-+ https://github.com/oesmith/gatt-xml/blob/master/org.bluetooth.characteristic.heart_rate_measurement.xml
-+ https://github.com/polarofficial/create-mobile-app-for-polar-sensors
-
-# Requirements
-
-Redis should be running prior to starting this module.
+- https://www.oreilly.com/library/view/getting-started-with/9781491900550/ch04.html
+- https://stackoverflow.com/questions/52882552/getting-heart-rate-variability-from-polar-h10-uwp
+- https://github.com/watsonix/node-ble-hr/blob/master/index.js
+- https://github.com/Lawreros/PolarStrapBLE/blob/master/Scenario2_Client.xaml.cs
+- https://www.raywenderlich.com/231-core-bluetooth-tutorial-for-ios-heart-rate-monitor
+- https://github.com/danielfppps/hbpimon/blob/master/hb.py
+- https://stackoverflow.com/questions/58778739/polar-h10-to-rpi-zero-w-hr-notifications-stop-after-ca-100s-without-error-mess
+- https://stackoverflow.com/questions/56330018/how-to-read-and-convert-bluetooth-characteristic-from-byte-data-to-proper-values
+- https://github.com/oesmith/gatt-xml/blob/master/org.bluetooth.characteristic.heart_rate_measurement.xml
+- https://github.com/polarofficial/create-mobile-app-for-polar-sensors
--- src/module/polarbelt/polarbelt.py
+++ src/module/polarbelt/polarbelt.py
@@ -20,7 +20,7 @@
 import os
 import sys
 import asyncio
-from bleak import BleakClient, discover
+from bleak import BleakClient, BleakScanner
 
 if hasattr(sys, 'frozen'):
     path = os.path.split(sys.executable)[0]
@@ -43,34 +43,42 @@ else:
 sys.path.append(os.path.join(path, '../../lib'))
 import EEGsynth
 
+UUID_HEARTRATE = "2A37"
+UUID_MODEL_NUMBER = "2A24"
 
 class PolarClient:
     def __init__(self, path, name):
 
         # configure and start the patch, this will parse the command-line arguments and the ini file
-        patch = EEGsynth.patch(name=name, path=path)
+        self.patch = EEGsynth.patch(name=name, path=path)
 
         # this shows the splash screen and can be used to track parameters that have changed
         self.monitor = EEGsynth.monitor(name=name, debug=self.patch.getint("general", "debug", default=1))
 
-        # BLE client.
+        # the address is a string like 818AFE80-3652-4DBA-5192-88128E5FAB3D
+        self.address = self.patch.getstring("input", "address")
+
+        # BLE client
         self.loop = asyncio.get_event_loop()
-        self.ble_client = BleakClient(self.patch.getstring("input", "uuid"), loop=self.loop)
+        self.ble_client = BleakClient(self.address, loop=self.loop)
+
+        # show a list of all available devices
         self.loop.run_until_complete(self.discover())
 
 
     async def discover(self):
-        devices = await discover()
+        devices = await BleakScanner.discover()
+        self.monitor.info("================= Available devices =================")
         for d in devices:
             self.monitor.info(d)
+        self.monitor.info("=====================================================")
 
 
     async def connect(self):
-        self.monitor.info("Trying to connect to Polar belt {0}".format(self.patch.getstring("input", "uuid")))
+        self.monitor.info("Trying to connect to Polar belt " + self.address)
         await self.ble_client.connect()
-        await self.ble_client.start_notify(self.patch.getstring("input", "uuid"),
-                                           self.data_handler)
-        self.monitor.success("Connected to Polar belt {0}".format(self.patch.getstring("input", "uuid")))
+        await self.ble_client.start_notify(UUID_HEARTRATE, self.data_handler)
+        self.monitor.success("Connected to Polar belt " + self.address)
 
 
     def start(self):
@@ -79,19 +87,19 @@ class PolarClient:
 
 
     def stop(self):
-        asyncio.ensure_future(self.ble_client.stop_notify(self.patch.getstring("input", "uuid")))
+        asyncio.ensure_future(self.ble_client.stop_notify(UUID_HEARTRATE))
         asyncio.ensure_future(self.ble_client.disconnect())
-        self.monitor.success("Disconnected from Polar belt {0}".format(self.patch.getstring("input", "uuid")))
+        self.monitor.success("Disconnected from Polar belt " + self.address)
 
 
-    def data_handler(self, sender, data):    # sender (UUID) unused but required by Bleak API
+    def data_handler(self, sender, data):  # sender is unused but required by Bleak API
         """
         data has up to 6 bytes:
         byte 1: flags
             00 = only HR
             16 = HR and IBI(s)
         byte 2: HR
-        byte 3 and 4: IBI1
+        byte 3 and 4: IBI1 (if present)
         byte 5 and 6: IBI2 (if present)
         byte 7 and 8: IBI3 (if present)
         etc.
@@ -112,12 +120,14 @@ class PolarClient:
             hr = data[1]
             for i in range(2, len(bytes), 2):
                 ibis.append(data[i] + 256 * data[i + 1])
-        if ibis:
-            for ibi in ibis:
-                self.patch.setvalue(self.patch.getstring("output", "key_ibi"), ibi)
         if hr:
-            self.patch.setvalue(self.patch.getstring("output", "key_hr"), hr)
-        print("Received HR={0}, IBI(s)={1}".format(hr, ibis))
+            key = self.patch.getstring('output', 'heartrate')
+            self.patch.setvalue(key, hr)
+        for ibi in ibis:
+            key = self.patch.getstring('output', 'interbeat')
+            self.patch.setvalue(key, ibi)
+
+        self.monitor.debug("hr={0}, ibis={1}".format(hr, ibis))
 
 
 if __name__ == "__main__":
